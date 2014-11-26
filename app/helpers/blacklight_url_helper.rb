d5f34c0fd6c79bfa0e0007d26d4d7be2564631ac
Richard Doe <richard.doe@rwdit.net>
2014-11-26 11:42:19 +0000

    Move overriden track_solr_document_path helper to ApplicationHelper.

--- app/helpers/blacklight_url_helper.rb
+++ app/helpers/blacklight_url_helper.rb
@@ -11,9 +11,4 @@ module BlacklightUrlHelper
     end
   end
 
-  # @todo: un-hard-code "catalog"
-  def track_solr_document_path(doc, options = {})
-    url_for(options.merge(controller: "catalog", action: :track, provider_id: doc.provider_id, record_id: doc.record_id))
-  end
-  
 end
