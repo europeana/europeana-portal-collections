9fc787d2284b0b5f1beb0ece7c59a241728e4ee6
Richard Doe <richard.doe@rwdit.net>
2014-11-26 13:38:15 +0000

    Move URL helper methods into UrlHelper module.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  # @todo: un-hard-code "catalog"
-  def track_solr_document_path(doc, options = {})
-    url_for(options.merge(controller: "catalog", action: :track, provider_id: doc.provider_id, record_id: doc.record_id))
-  end
 end
---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,14 @@
+module UrlHelper
+  def track_solr_document_path(doc, options = {})
+    url_for(options.merge(controller: "catalog", action: :track, provider_id: doc.provider_id, record_id: doc.record_id))
+  end
+  
+  def polymorphic_url(record_or_hash_or_array, options = {})
+    if record_or_hash_or_array.is_a?(SolrDocument)
+      doc = record_or_hash_or_array
+      solr_document_url(options.merge(provider_id: doc.provider_id, record_id: doc.record_id))
+    else
+      super
+    end
+  end
+end
