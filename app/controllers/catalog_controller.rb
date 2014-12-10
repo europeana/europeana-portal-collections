7652069b7f58902c03365b6a1c9d500fa96e73a0
Richard Doe <richard.doe@rwdit.net>
2014-12-10 09:43:03 +0000

    Do not display channel query in front-end.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,11 +6,29 @@ class CatalogController < ApplicationController
   def channel
     channel_id = params[:id]
     
+    user_params = params.dup
     if channel_query = Channels::Application.config.channels[channel_id.to_sym][:query]
-      params[:q] = params[:q] ? "(#{channel_query}) AND #{params[:q]}" : channel_query
+      user_params[:q] = user_params[:q] ? "(#{channel_query}) AND #{params[:q]}" : channel_query
     end
     
-    index
-    render :action => 'index'
+    (@response, @document_list) = get_search_results(user_params)
+      
+    respond_to do |format|
+      format.html { render :action => 'index' }
+      format.rss  { render :action => 'index', :layout => false }
+      format.atom { render :action => 'index', :layout => false }
+      format.json do
+        render json: render_search_results_as_json
+      end
+
+      additional_response_formats(format)
+      document_export_formats(format)
+    end
+  end
+  
+  # Channels always have search parameters, even if none are entered by the user
+  # @see Blacklight::Catalog#has_search_parameters?
+  def has_search_parameters?
+    true
   end
 end
