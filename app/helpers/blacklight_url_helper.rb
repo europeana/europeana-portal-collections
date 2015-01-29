c52edcb1a96f9b6582c16f1fa518b236b59a272e
Richard Doe <richard.doe@rwdit.net>
2015-01-29 10:09:36 +0000

    Add .html to record URLs.

--- app/helpers/blacklight_url_helper.rb
+++ app/helpers/blacklight_url_helper.rb
@@ -3,7 +3,7 @@ module BlacklightUrlHelper
   
   def url_for_document(doc, options = {})
     if doc.respond_to?(:provider_id) and doc.respond_to?(:record_id)
-      solr_document_path(provider_id: doc.provider_id, record_id: doc.record_id)
+      solr_document_path(provider_id: doc.provider_id, record_id: doc.record_id, format: 'html')
     else
       super
     end
