ce0a696091d2c409c7c8d6d6e3dd4ee52c51e398
Richard Doe <richard.doe@rwdit.net>
2015-03-09 17:26:58 +0000

    Re #64: Support filtering of searches by additional keywords.

--- app/helpers/render_constraints_helper.rb
+++ app/helpers/render_constraints_helper.rb
@@ -5,6 +5,17 @@
 module RenderConstraintsHelper
   include Blacklight::RenderConstraintsHelperBehavior
 
+  def query_has_constraints?(localized_params = params)
+    !(localized_params[:q].blank? && localized_params[:f].blank? &&
+      localized_params[:qf].blank?)
+  end
+
+  def render_constraints(localized_params = params)
+    render_constraints_query(localized_params) +
+      render_constraints_filters(localized_params) +
+      render_constraints_qfs(localized_params)
+  end
+
   # Based on
   # {Blacklight::RenderConstraintsHelperBehavior#render_constraints_query}
   # but removed :action => :index param from remove link
@@ -18,6 +29,23 @@ module RenderConstraintsHelper
       localized_params[:q],
       classes: ['query'],
       remove: scope.url_for(localized_params.merge(q: nil))
+    )
+  end
+
+  def render_constraints_qfs(localized_params = params)
+    return "".html_safe unless localized_params[:qf]
+     content = []
+     localized_params[:qf].each do |value|
+       content << render_qf_element(value, localized_params)
+     end
+
+     safe_join(content.flatten, "\n")
+  end
+
+  def render_qf_element(value, localized_params)
+    render_constraint_element(nil, value,
+      :remove => search_action_path(remove_qf_param(value, localized_params)),
+      :classes => ['filter']
     )
   end
 end
