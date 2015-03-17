043af0b64e01e069d10b88a6a12b6d744fbaa369
Andy MacLean <andyjmaclean@gmail.com>
2015-03-17 19:22:56 +0100

    Integration with mustache Gem

--- app/controllers/concerns/europeana_catalog.rb
+++ app/controllers/concerns/europeana_catalog.rb
@@ -141,16 +141,136 @@ module EuropeanaCatalog
   end
   
   def fix_model
+    
+    @pagetitle     = params['q']
     @searchresults = @document_list
-    @pagetitle = params['q']
-    @headmeta = [
-      {"name":"HandheldFriendly",   "content": "True"},
-      {"name":"X-UA-Compatible",    "content": "IE=edge"},
-      {"name":"viewport",           "content": "width=device-width,initial-scale=1.0"},        
-      {"httpequiv":"Content-Type",  "content": "text/html; charset=utf-8" },
-      {"name":"csrf-param",         "content": "authenticity_token"},
-      {"name":"csrf-token",         "content": form_authenticity_token }
+    
+    @head_meta = [
+      #{'name':'X-UA-Compatible',    'content': 'IE=edge'},
+      #{'name':'viewport',           'content': 'width=device-width,initial-scale=1.0'},        
+      {'meta_name':'HandheldFriendly',   'content': 'True'},
+      {'httpequiv':'Content-Type',       'content': 'text/html; charset=utf-8' },
+      {'meta_name':'csrf-param',         'content': 'authenticity_token'},
+      {'meta_name':'csrf-token',         'content': form_authenticity_token }
     ]
+
+    @form_action_search = request.protocol + request.host_with_port + '/'
+      
+    @head_links = [
+      {'rel':'search',        'type':'application/opensearchdescription+xml', 'href': request.host_with_port + '/catalog/opensearch.xml', 'title':'Blacklight'},
+      {'rel':'shortcut icon', 'type':'image/x-icon',                          'href': 'assets/favicon.ico'},
+      {'rel':'stylesheet',     'href':'/assets/blacklight.css',               'media':'all'},
+      {'rel':'stylesheet',     'href':'/assets/europeana.css',                'media':'all'},
+      {'rel':'stylesheet',     'href':'/assets/application.css',              'media':'all'}
+    ]
+    
+    
+    @input_search = {
+      'title': 'Search',
+      'input_name':  'q',
+      'input_value': params['q'] ? params['q'] : '',
+      'placeholder': 'Add a search term'
+    }
+
+    # All theses are blacklight's dependencies - getting the via the helper would be nicer    
+    @js_files = [
+      {'path': 'assets/jquery.js'},
+      {'path': 'assets/turbolinks.js'},
+      {'path': 'assets/blacklight/core.js'},
+      {'path': 'assets/blacklight/autofocus.js'},
+      {'path': 'assets/blacklight/checkbox_submit.js'},
+      {'path': 'assets/blacklight/bookmark_toggle.js'},
+      {'path': 'assets/blacklight/ajax_modal.js'},
+      {'path': 'assets/blacklight/search_context.js'},
+      {'path': 'assets/blacklight/collapsable.js'},
+      
+      {'path': 'assets/bootstrap/transition.js'},
+      {'path': 'assets/bootstrap/collapse.js'},
+      {'path': 'assets/bootstrap/dropdown.js'},
+      {'path': 'assets/bootstrap/alert.js'},
+      {'path': 'assets/bootstrap/modal.js'},
+        
+      {'path': 'assets/blacklight/blacklight.js'}
+    ]
+    
+    
+    @menus= {
+        'actions': {
+          'button-title':'Actions',
+          'menu_id': 'dropdown-result-actions',
+          'menu-title': 'Save to:',
+          'items': [
+            {
+              'url':'http://europeana.eu',
+              'text': 'First Item'
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Another Label'
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Label here'
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Fourth Item'
+            },
+            {
+              'divider': true
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Another Label',
+              'calltoaction': true
+            },
+            {
+              'divider': true
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Another Label',
+              'calltoaction': true
+            }
+          ]
+        },
+        'sort': {
+          'button-title':'Relevance',
+          'menu_id': 'dropdown-result-sort',
+          'menu-title': 'Sort by:',
+          'items': [
+            {
+              'text': 'Date',
+              'url':'http://europeana.eu'
+            },
+            {
+              'text': 'Alphabetical',
+              'url':'http://europeana.eu'
+            },
+            {
+              'text': 'Relevance',
+              'url':'http://europeana.eu'
+            },
+            {
+              'divider': true
+            },
+            {
+              'url':'http://europeana.eu',
+              'text': 'Another Label',
+              'calltoaction': true
+            },
+            {
+              'divider': true
+            },
+            {
+              'text': 'Advanced Search',
+              'url':'http://europeana.eu',
+              'calltoaction': true
+            }
+          ]
+        } 
+      }
+
     
   end
   
