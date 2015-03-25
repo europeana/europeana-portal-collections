bd3cfb8d93d86c36371b56c804d6944caeadfeab
Richard Doe <richard.doe@rwdit.net>
2015-03-25 12:07:19 +0000

    Re #63: Check for presence of range param, don't assume it.

--- app/models/europeana/blacklight/search_builder/ranges.rb
+++ app/models/europeana/blacklight/search_builder/ranges.rb
@@ -11,6 +11,7 @@ module Europeana
         end
 
         def add_range_qf_to_api(api_parameters)
+          return unless blacklight_params.key?(:range) && blacklight_params[:range].is_a?(Hash)
           blacklight_params[:range].each_pair do |range_field, range_values|
             api_parameters[:qf] ||= []
             api_parameters[:qf] << "#{range_field}:[#{range_values[:begin]} TO #{range_values[:end]}]"
