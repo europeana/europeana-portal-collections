d53ce8a5fec77b7bb1afc65bc090272a6c58f233
Richard Doe <richard.doe@rwdit.net>
2015-03-27 10:41:50 +0000

    Make MustacheHelper::Translator respond to #fetch, for missing locale keys.

--- app/helpers/mustache_helper.rb
+++ app/helpers/mustache_helper.rb
@@ -3,13 +3,19 @@ module MustacheHelper
     def [](key)
       I18n.translate(key)
     end
+
     def to_hash
       self
     end
+
     def key?(key)
       I18n.exists?(key)
     end
     alias_method :has_key?, :key?
+
+    def fetch(key, default = nil)
+      self[key] || default
+    end
   end
 
   def i18n
