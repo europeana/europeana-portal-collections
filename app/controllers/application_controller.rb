d176e1b12e7f1ee8348ddb7c6810e8eefd8c190e
Andy MacLean <andyjmaclean@gmail.com>
2015-05-07 14:15:25 +0200

    merge 161

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
    
   def set_locale
-   # I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
   
 end
