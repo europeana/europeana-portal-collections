222e19cc0370d8bea6607e197fb5fe366b1c4344
Richard Doe <richard.doe@rwdit.net>
2015-06-24 12:17:14 +0100

    Re #188: Update specs for API requests.

--- spec/webmock_helper.rb
+++ spec/webmock_helper.rb
@@ -5,6 +5,7 @@ require 'support/europeana_blog_helper'
 RSpec.configure do |config|
   config.before(:each) do
     ENV['EUROPEANA_API_KEY'] = 'test'
+    WebMock.disable_net_connect!(:allow_localhost => true) # for poltergeist
   end
 
   config.include EuropeanaAPIHelper, type: :controller
