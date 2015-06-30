2add50d793aee750477286798cf6d31f68ca045e
Richard Doe <richard.doe@rwdit.net>
2015-06-30 09:32:42 +0100

    Re #188: Make Capybara driver configurable by env

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -1,37 +1,32 @@
 # Capybara integration
 require 'capybara/rspec'
 require 'capybara/rails'
-require 'capybara/poltergeist'
-require 'selenium-webdriver'
 
-if (true) # false for local non-headless testing
-
+if ENV['CAPYBARA_DRIVER'] == 'selenium'
+  require 'selenium-webdriver'
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: (ENV['CAPYBARA_BROWSER'] || :firefox).to_sym)
+  end
+  Capybara.default_driver = :selenium
+  Capybara.javascript_driver = :selenium
+else
+  require 'capybara/poltergeist'
   Capybara.register_driver :poltergeist do |app|
     options = {
       js_errors: true,
       timeout: 120,
       debug: false,
-      #phantomjs_options: ['--load-images=no', '--disk-cache=false'],
       inspector: true,
       ignore_ssl_errors: true
     }
     Capybara::Poltergeist::Driver.new(app, options)
   end
-  
+
   Capybara.javascript_driver = :poltergeist
   Capybara.default_selector  = :css
-  
+
   RSpec.configure do |config|
     # Include Capybara for integration testing.
     config.include Capybara::DSL
   end
-
-else
-  
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :firefox)
-  end
-  Capybara.default_driver = :selenium
-  Capybara.javascript_driver = :selenium
-
 end
