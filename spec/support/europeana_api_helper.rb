ee486a939d95c1d5ec67b9cd28aca6446d8f66dc
Richard Doe <richard.doe@rwdit.net>
2015-07-15 12:29:19 +0100

    Unbreak all the specs.

--- spec/support/europeana_api_helper.rb
+++ spec/support/europeana_api_helper.rb
@@ -9,14 +9,14 @@ module EuropeanaAPIHelper
       end.join(',')
 
       stub_request(:get, Europeana::API.url + '/search.json').
-        with(query: hash_including(wskey: api_key)).
+        with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY'])).
         to_return(body: '{"success":true,"itemsCount":' + items.size.to_s + ',"totalResults":' + items.size.to_s + ',"items":[' + items + ']}',
                   status: 200,
                   headers: { 'Content-Type' => 'text/json' })
 
       # API Record
       stub_request(:get, %r{#{Europeana::API.url}/record/[^/]+/[^/]+.json}).
-        with(query: hash_including(wskey: api_key)).
+        with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY'])).
         to_return do |request|
           id = request.uri.path.match(%r{/record(/[^/]+/[^/]+).json})[1]
           {
@@ -28,7 +28,7 @@ module EuropeanaAPIHelper
 
       # Hierarchy API
       stub_request(:get, %r{#{Europeana::API.url}/record/[^/]+/[^/]+/(self|parent|children|ancestor-self-siblings|precee?ding-siblings|following-siblings).json}).
-        with(query: hash_including(wskey: api_key)).
+        with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY'])).
         to_return(body: '{"success":false,"message":"This record has no hierarchical structure!"}',
                   status: 200,
                   headers: { 'Content-Type' => 'text/json' })
@@ -40,23 +40,19 @@ module EuropeanaAPIHelper
     end
   end
 
-  def api_key
-    ENV['EUROPEANA_API_KEY']
-  end
-
   def an_api_search_request
     a_request(:get, Europeana::API.url + '/search.json').
-      with(query: hash_including(wskey: api_key))
+      with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY']))
   end
 
   def an_api_record_request_for(id)
     a_request(:get, Europeana::API.url + "/record#{id}.json").
-      with(query: hash_including(wskey: api_key))
+      with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY']))
   end
 
   def an_api_hierarchy_request_for(id)
     a_request(:get, %r{#{Europeana::API.url}/record#{id}/(self|parent|children|ancestor-self-siblings|precee?ding-siblings|following-siblings).json}).
-      with(query: hash_including(wskey: api_key))
+      with(query: hash_including(wskey: ENV['EUROPEANA_API_KEY']))
   end
 
   def a_media_proxy_request_for(id)
