25dd74630e1e7333e8487d1b686a308603a47c9e
andyjmaclean <andyjmaclean@gmail.com>
2015-07-16 17:42:59 +0200

    Re: #308 removed extra click

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -39,7 +39,7 @@ RSpec.feature 'Search page', :type => :feature do
       fill_in('q', with: 'paris')
       find('button.search-submit').click
 
-      find('button.search-submit').click
+      #find('button.search-submit').click
 
       expect(page.all('li.search-tag').size).to eq(1)
     end
