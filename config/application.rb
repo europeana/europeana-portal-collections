b274188447f9d9b129f735b8c6e934236bd3da97
Richard Doe <richard.doe@rwdit.net>
2015-07-16 11:10:32 +0100

    Move custom routes module into app/routes

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Europeana
       # -- all .rb files in that directory are automatically loaded.
 
       # Load job classes
-      config.autoload_paths += %W(#{config.root}/app/jobs)
+      config.autoload_paths += %W(#{config.root}/app/jobs #{config.root}/app/routes)
 
       # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
       # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
