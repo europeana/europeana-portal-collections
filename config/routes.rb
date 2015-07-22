4e5c78bff0a975300784c18df206adb0231bd638
Richard Doe <richard.doe@rwdit.net>
2015-07-22 08:51:16 +0100

    Re #337: Redirect from root to relative URL root if set.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Blacklight::Routes.send(:include, BlacklightRoutes)
 
 Rails.application.routes.draw do
+  unless ENV['RAILS_RELATIVE_URL_ROOT'].blank?
+    get '/', to: redirect(ENV['RAILS_RELATIVE_URL_ROOT'])
+  end
+
   scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do
     root to: 'home#index'
     get 'search', to: 'portal#index'
