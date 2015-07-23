72a1ab2ab1a5626e95ed82acc0bcd977f2448a03
Richard Doe <richard.doe@rwdit.net>
2015-07-23 14:37:24 +0100

    Re #335: Minify HTML

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,29 @@ module Europeana
       # Compress HTTP responses
       config.middleware.use Rack::Deflater
 
+      # Minify HTML
+      config.middleware.use HtmlCompressor::Rack, {
+        enabled: true,
+        remove_multi_spaces: true,
+        remove_comments: true,
+        remove_intertag_spaces: false,
+        remove_quotes: false,
+        compress_css: false,
+        compress_javascript: false,
+        simple_doctype: false,
+        remove_script_attributes: false,
+        remove_style_attributes: false,
+        remove_link_attributes: false,
+        remove_form_attributes: false,
+        remove_input_attributes: false,
+        remove_javascript_protocol: false,
+        remove_http_protocol: false,
+        remove_https_protocol: false,
+        preserve_line_breaks: false,
+        simple_boolean_attributes: false,
+        compress_js_templates: false
+      }
+
       # Load job classes
       config.autoload_paths += %W(#{config.root}/app/jobs #{config.root}/app/routes)
 
