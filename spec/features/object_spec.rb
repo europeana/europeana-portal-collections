174dd24b57142d5c86850fcc1e79a3b07111306a
Richard Doe <richard.doe@rwdit.net>
2015-07-24 10:30:30 +0100

    Extend feature specs with JS/non-JS tests

--- spec/features/object_spec.rb
+++ spec/features/object_spec.rb
@@ -1,42 +1,67 @@
-RSpec.feature 'Object page', :type => :feature do
+RSpec.feature 'Object page' do
   describe 'Navigation' do
-    it 'expects working previous / next links', js: true do
-      visit '/'
+    context 'with JS', js: true do
+      it 'expects working previous / next links' do
+        visit '/'
 
-      sleep 3
+        sleep 3
 
-      fill_in('q', with: 'paris')
+        fill_in('q', with: 'paris')
 
-      expect(page).to have_css('.searchbar button.search-submit')
+        expect(page).to have_css('.searchbar button.search-submit')
 
-      find('.searchbar button.search-submit').click
+        find('.searchbar button.search-submit').click
 
-      sleep 3
+        sleep 3
 
-      find('.results-list ol.result-items li:first-child h1 a').click
+        find('.results-list ol.result-items li:first-child h1 a').click
 
-      sleep 3
+        sleep 3
 
-      expect(page).to have_selector('.next')
+        expect(page).to have_css('.next')
 
-      page_title_1 = page.title
+        page_title_1 = page.title
 
-      find('.next a').click
+        find('.next a').click
 
-      sleep 3
+        sleep 3
 
-      page_title_2 = page.title
+        page_title_2 = page.title
 
-      expect(page).to have_css('.next a')
-      expect(page).to have_css('.previous a')
+        expect(page).to have_css('.next a')
+        expect(page).to have_css('.previous a')
 
-      expect(page_title_1).not_to eq(page_title_2)
+        expect(page_title_1).not_to eq(page_title_2)
 
-      find('.previous a').click
+        find('.previous a').click
 
-      sleep 3
+        sleep 3
 
-      expect(page.title).to eq(page_title_1)
+        expect(page.title).to eq(page_title_1)
+      end
+    end
+
+    context 'without JS', js: false do
+      it 'expects no working previous / next links' do
+        visit '/'
+
+        sleep 3
+
+        fill_in('q', with: 'paris')
+
+        expect(page).to have_css('.searchbar button.search-submit')
+
+        find('.searchbar button.search-submit').click
+
+        sleep 3
+
+        page.all('.results-list ol.result-items li h1 a')[2].click
+
+        sleep 3
+
+        expect(page).not_to have_css('.next')
+        expect(page).not_to have_css('.previous')
+      end
     end
   end
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,55 +1,59 @@
 require 'rails_helper'
 
-RSpec.feature 'Search page', :type => :feature do
-  describe 'search page' do
-    it 'expects results' do
-      visit '/'
+RSpec.feature 'Search page' do
+  [false, true].each do |js|
+    context (js ? 'with JS' : 'without JS'), js: js do
+      describe 'search page' do
+        it 'expects results' do
+          visit '/'
 
-      sleep 3
+          sleep 3
 
-      expect(page).to have_css('input[name=q]')
+          expect(page).to have_css('input[name=q]')
 
-      fill_in('q', with: 'paris')
+          fill_in('q', with: 'paris')
 
-      expect(page).to have_css('.searchbar button.search-submit')
+          expect(page).to have_css('.searchbar button.search-submit')
 
-      find('.searchbar button.search-submit').click
+          find('.searchbar button.search-submit').click
 
-      sleep 2
+          sleep 2
 
-      expect(page.all('.results-list').size).to be > (0)
+          expect(page.all('.results-list').size).to be > (0)
 
-      expect(page.all('.results-list ol.result-items li').size).to be_between(1, 24)
-    end
+          expect(page.all('.results-list ol.result-items li').size).to be_between(1, 24)
+        end
 
-    it 'permits empty searches' do
-      visit '/'
+        it 'permits empty searches' do
+          visit '/'
 
-      fill_in('q', with: '')
-      find('button.search-submit').click
+          fill_in('q', with: '')
+          find('button.search-submit').click
 
-      path_root = ENV['RAILS_RELATIVE_URL_ROOT'] || ''
-      expect(current_path).to eq(path_root + '/search')
-    end
+          path_root = ENV['RAILS_RELATIVE_URL_ROOT'] || ''
+          expect(current_path).to eq(path_root + '/search')
+        end
 
-    it 'ignores 2nd empty search' do
-      visit '/'
+        it 'ignores 2nd empty search' do
+          visit '/'
 
-      fill_in('q', with: 'paris')
-      find('button.search-submit').click
+          fill_in('q', with: 'paris')
+          find('button.search-submit').click
 
-      expect(page.all('li.search-tag').size).to eq(1)
-    end
+          expect(page.all('li.search-tag').size).to eq(1)
+        end
 
-    it 'does not submit placeholder text' do
-      visit '/'
+        it 'does not submit placeholder text' do
+          visit '/'
 
-      fill_in('q', with: '')
-      find('button.search-submit').click
+          fill_in('q', with: '')
+          find('button.search-submit').click
 
-      placeholder = find('.searchbar input.search-input')[:placeholder]
+          placeholder = find('.searchbar input.search-input')[:placeholder]
 
-      expect(page.all('li.search-tag', text: placeholder)).to be_blank
+          expect(page.all('li.search-tag', text: placeholder)).to be_blank
+        end
+      end
     end
   end
 end
