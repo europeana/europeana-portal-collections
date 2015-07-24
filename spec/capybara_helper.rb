57e477a2e8c5bd1f372109d5adec0f45fdabb5dd
Richard Doe <richard.doe@rwdit.net>
2015-07-24 12:41:19 +0100

    Increase poltergeist's window size.

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -25,7 +25,8 @@ else
       phantomjs_options: [
         '--local-to-remote-url-access=true'
       ],
-      js_errors: true
+      js_errors: true,
+      window_size: [1600, 1200]
     )
   end
 
