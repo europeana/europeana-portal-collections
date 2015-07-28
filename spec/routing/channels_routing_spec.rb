98cdbc174ef8212d2f1e7ff6939842fe035569c1
Richard Doe <richard.doe@rwdit.net>
2015-07-28 11:19:57 +0200

    Fix request and routing specs for relative URL root.

--- spec/routing/channels_routing_spec.rb
+++ spec/routing/channels_routing_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'routes for the channels controller', :type => :routing do
   it 'routes GET /channels to channels#index' do
-    expect(get('/channels')).to route_to('channels#index')
+    expect(get(ENV['RAILS_RELATIVE_URL_ROOT'] + '/channels')).to route_to('channels#index')
   end
 
   it 'routes GET /channels/:id to channels#show' do
-    expect(get('/channels/art')).to route_to('channels#show', id: 'art')
+    expect(get(ENV['RAILS_RELATIVE_URL_ROOT'] + '/channels/art')).to route_to('channels#show', id: 'art')
   end
 end
--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'routes for the home controller', :type => :routing do
   it 'routes GET / to home#index' do
-    expect(get('/')).to route_to('home#index')
+    expect(get(ENV['RAILS_RELATIVE_URL_ROOT'] + '/')).to route_to('home#index')
   end
 end
