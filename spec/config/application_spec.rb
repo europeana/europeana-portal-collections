dbae2ff1ad688bdf5a866007b79c2301d3c19443
Richard Doe <richard.doe@rwdit.net>
2015-08-26 09:03:18 +0100

    Remove redundant channel config initializer

--- spec/config/application_spec.rb
+++ spec/config/application_spec.rb
@@ -1,10 +1,6 @@
 describe Europeana::Portal::Application, 'configuration' do
   let(:config) { described_class.config }
 
-  it 'has channels config' do
-    expect(config.x.channels).not_to be_blank
-  end
-
   it 'sets paperclip defaults' do
     expect(config.paperclip_defaults[:styles]).to have_key(:small)
   end
