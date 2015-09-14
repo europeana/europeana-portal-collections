835cbaf5f121948ad6b3cceaab9935b0e30dfb46
Richard Doe <richard.doe@rwdit.net>
2015-09-14 11:41:08 +0100

    Update translator spec.

--- spec/helpers/mustache_helper/translator_spec.rb
+++ spec/helpers/mustache_helper/translator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe View::Translator, type: :model do
-  subject { View::Translator.new(scope) }
+RSpec.describe MustacheHelper::Translator, type: :model do
+  subject { described_class.new(scope) }
   let(:scope) { { count: 10 } }
 
   let(:locale) { :en }
