1a59ba9e620435227ec1c823fe439e95dedd9436
Richard Doe <richard.doe@rwdit.net>
2015-09-15 11:35:43 +0100

    Meta description for static pages

--- app/views/portal/static.rb
+++ app/views/portal/static.rb
@@ -13,20 +13,26 @@ module Portal
       end
     end
 
+    def head_meta
+      [
+        { meta_name: 'description', content: strip_tags(Nokogiri::HTML(content[:text]).xpath('//p').first.text) }
+      ] + helpers.head_meta
+    end
+
     private
 
     def content_about
       {
         title: t('site.pages.about.title'),
         text: t('site.pages.about.text')
-      }.merge(helpers.content)
+      }.reverse_merge(helpers.content)
     end
 
     def content_channels_music_about
       {
         title: t('site.pages.music-channel-about.title'),
         text: t('site.pages.music-channel-about.text')
-      }.merge(helpers.content)
+      }.reverse_merge(helpers.content)
     end
   end
 end
