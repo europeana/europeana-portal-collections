d1d38fdacdccbf32f3b0a74f3287f4d1590f2358
Richard Doe <richard.doe@rwdit.net>
2015-09-23 09:45:27 +0100

    Improve language settings controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -15,23 +15,27 @@ class SettingsController < ApplicationController
   # PUT language
   def update_language
     locales = set_locale_from_param
-    
+
     respond_to do |format|
-      format.html do
-        render action: :language, status: status_from_flash
-      end
+      format.html { render action: :language, status: flash_status }
       format.json do
-        render json: { success: !flash.key?(:alert), refresh: (locales.first != locales.last),
-                       message: flash.key?(:alert) ? flash.now[:alert] : flash.now[:notice] },
-               status: status_from_flash
+        render json: flash_json.merge({ refresh: (locales.first != locales.last) }),
+               status: flash_status
       end
     end
   end
 
   protected
 
-  def status_from_flash
+  def flash_status
     flash.key?(:alert) ? 400 : 200
+  end
+
+  def flash_json
+    {
+      success: !flash.key?(:alert),
+      message: flash.key?(:alert) ? flash.now[:alert] : flash.now[:notice]
+    }
   end
 
   ##
@@ -41,17 +45,12 @@ class SettingsController < ApplicationController
   #   current session locale
   def set_locale_from_param
     session_locale_was = session[:locale]
-
-    if params[:locale]
-      if I18n.available_locales.map(&:to_s).include?(params[:locale])
-        I18n.locale = params[:locale]
-        session[:locale] = I18n.locale
-      else
-        flash.now[:alert] = 'Invalid language specified. Available languages: ' + I18n.available_locales.map(&:to_s).join(',')
-      end
-    end
-    flash.now[:notice] = 'Language settings saved.' unless flash.key?(:alert)
-
-    [session_locale_was, session[:locale]]
+    I18n.locale = params[:locale] if params[:locale]
+    flash.now[:notice] = t('site.settings.language.flash.notice')
+  rescue I18n::InvalidLocale
+    flash.now[:alert] = t('site.settings.language.flash.alert', locales: I18n.available_locales.map(&:to_s).join(','))
+  ensure
+    session[:locale] = I18n.locale
+    return [session_locale_was, session[:locale]]
   end
 end
