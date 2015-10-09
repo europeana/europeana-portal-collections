1b52f6578cdcb329fcd362d25abce6bed2c6fb66
Richard Doe <richard.doe@rwdit.net>
2015-10-09 13:07:02 +0100

    Re #511: Colour browse page

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -4,6 +4,17 @@ class BrowseController < ApplicationController
   include Catalog
   include Europeana::Styleguide
 
+  # GET /browse/colours
+  def colours
+    params = { query: '*:*', rows: 0, profile: 'minimal facets' }
+    response = repository.search(params)
+    @colours = response.aggregations['COLOURPALETTE'].items
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
   # GET /browse/newcontent
   def new_content
     @providers = Rails.cache.fetch('browse/new_content/providers') || []
