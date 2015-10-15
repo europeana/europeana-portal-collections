981df20f0080f3ee0db8161092f790051eb7df52
Richard Doe <richard.doe@rwdit.net>
2015-10-15 17:23:51 +0100

    Re #608: Generate view class data once, return it many times.

--- app/views/pages/errors/not_found.rb
+++ app/views/pages/errors/not_found.rb
@@ -2,10 +2,12 @@ module Pages
   module Errors
     class NotFound < Portal::Static
       def content
-        {
-          intro: @page.title,
-          text: @page.body
-        }.reverse_merge(super)
+        @mustache[:content] ||= begin
+          {
+            intro: @page.title,
+            text: @page.body
+          }.reverse_merge(super)
+        end
       end
     end
   end
