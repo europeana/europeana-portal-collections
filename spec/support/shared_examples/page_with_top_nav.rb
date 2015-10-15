8a67a0d580a5204d716515b2d13d583f71a85309
Richard Doe <richard.doe@rwdit.net>
2015-10-15 12:00:00 +0100

    Style.

--- spec/support/shared_examples/page_with_top_nav.rb
+++ spec/support/shared_examples/page_with_top_nav.rb
@@ -1,11 +1,11 @@
 RSpec.shared_examples 'page with top nav' do
   it 'should have top nav link to home' do
     render
-    expect(rendered).to have_selector("#main-menu a", text: 'Home')
+    expect(rendered).to have_selector('#main-menu a', text: 'Home')
   end
 
   it 'should have top nav links to published channels' do
     render
-    expect(rendered).to have_selector("#main-menu a[href$=\"/channels/music\"]", text: 'Europeana Music')
+    expect(rendered).to have_selector('#main-menu a[href$="/channels/music"]', text: 'Europeana Music')
   end
 end
