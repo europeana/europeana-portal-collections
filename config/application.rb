b3ac4ff60e8103f8c46c82dd9fd6436a544d7b17
andyjmaclean <andyjmaclean@gmail.com>
2015-10-28 00:54:52 +0100

    Added latest locales

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Europeana
       # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
       config.i18n.default_locale = :en
       config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
-      config.i18n.available_locales = [:en, :nl]
+      config.i18n.available_locales = [:en, :nl, :de]
       config.i18n.fallbacks = true
 
       # Do not swallow errors in after_commit/after_rollback callbacks.
