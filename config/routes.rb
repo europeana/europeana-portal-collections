a876aa687782404a38ff96db16fdb64e48adc403
Richard Doe <richard.doe@rwdit.net>
2015-10-28 10:10:40 +0000

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   blacklight_for :portal
 
-  resources :channels, only: [:show, :index]
+  resources :collections, only: [:show, :index]
   resources :landing_pages, only: [:show]
 
   mount RailsAdmin::Engine => '/cms', as: 'rails_admin'
