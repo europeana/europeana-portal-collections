4a06ff3cd4eb1eddba4876cb05043ac36075cd1d
Richard Doe <richard.doe@rwdit.net>
2015-11-05 12:38:53 +0000

    Fix regression

--- app/views/portal/show.rb
+++ app/views/portal/show.rb
@@ -707,7 +707,7 @@ module Portal
 
     def creator_title
       document.fetch('agents.prefLabel', []).first ||
-        render_document_show_field_value(document, 'dcCreator')
+        render_document_show_field_value(document, 'dcCreator') ||
         render_document_show_field_value(document, 'proxies.dcCreator')
     end
 
