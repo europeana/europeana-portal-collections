e9da4110455233699db4ad3282b801f6a4023b36
andyjmaclean <andyjmaclean@gmail.com>
2015-12-09 17:26:47 +0100

    Re: #753 Added languages

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Europeana
       # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
       config.i18n.default_locale = :en
       config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
-      config.i18n.available_locales = [:bg, :de, :el, :en, :es, :fr, :hu, :it, :nl, :pl, :pt, :ro, :ru, :sv]
+      config.i18n.available_locales = [:bg, :ca, :de, :el, :en, :es, :fi, :fr, :hu, :it, :lt, :nl, :pl, :pt, :ro, :ru, :sv]
       config.i18n.fallbacks = true
 
       # Do not swallow errors in after_commit/after_rollback callbacks.
