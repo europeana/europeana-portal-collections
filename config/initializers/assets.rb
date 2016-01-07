0acfc48af59ecc8b8a33491515c87ef8d41ebbdb
Richard Doe <richard.doe@rwdit.net>
2016-01-07 14:32:39 +0000

    Prefer detect over find.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = Bundler.environment.dependencies.find { |d| d.name == 'europeana-styleguide' }.source.ref
+Rails.application.config.assets.version = Bundler.environment.dependencies.detect { |d| d.name == 'europeana-styleguide' }.source.ref
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
