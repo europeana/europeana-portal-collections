a7f4b43c3c2fbc651aaaaed75b6df25b9d13f65a
Richard Doe <richard.doe@rwdit.net>
2016-01-07 14:27:53 +0000

    Scheduled job to retrieve and cache colour facets

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -5,11 +5,9 @@ class BrowseController < ApplicationController
   include Europeana::Styleguide
 
   # GET /browse/colours
-  # @todo Get colours with a scheduled job and cache them
+  # @todo Load @colours from view helper, to bypass if HTML cached
   def colours
-    params = { query: '*:*', rows: 0, profile: 'minimal facets' }
-    response = repository.search(params)
-    @colours = response.aggregations['COLOURPALETTE'].items
+    @colours = Rails.cache.fetch('browse/colours/facets') || []
 
     respond_to do |format|
       format.html
@@ -17,7 +15,7 @@ class BrowseController < ApplicationController
   end
 
   # GET /browse/newcontent
-  # @todo Load @providers from view helper, to bypass if cached
+  # @todo Load @providers from view helper, to bypass if HTML cached
   def new_content
     @providers = Rails.cache.fetch('browse/new_content/providers') || []
 
@@ -27,7 +25,7 @@ class BrowseController < ApplicationController
   end
 
   # GET /browse/sources
-  # @todo Load @providers from view helper, to bypass if cached
+  # @todo Load @providers from view helper, to bypass if HTML cached
   def sources
     @providers = Rails.cache.fetch('browse/sources/providers') || []
     @providers.each do |provider|
