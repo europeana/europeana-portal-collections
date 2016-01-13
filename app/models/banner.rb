ea2107a2fa0a3ef7d68f047e6ef06c5e8c5bd5be
Richard Doe <richard.doe@rwdit.net>
2016-01-13 09:04:39 +0000

    Re #628: Make phase feedback banner touch published pages
    To invalidate their cached versions as all display it.

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -13,4 +13,23 @@ class Banner < ActiveRecord::Base
 
   translates :title, :body, fallbacks_for_empty_translations: true
   accepts_nested_attributes_for :translations, allow_destroy: true
+
+  after_update :touch_pages, if: :phase_feedback_banner?
+  after_touch :touch_pages, if: :phase_feedback_banner?
+
+  ##
+  # All published pages display the phase feedback banner; touch them to
+  #   invalidate cache
+  def touch_pages
+    Page.published.find_each do |p|
+      p.touch
+    end
+  end
+
+  ##
+  # @todo this is inelegant, does not belong in the model; devise a proper
+  #   banner to page association
+  def phase_feedback_banner?
+    key == 'phase-feedback'
+  end
 end
