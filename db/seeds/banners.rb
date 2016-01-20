c85134c9e461c0414eec165f50eb3478eb632ff7
Richard Doe <richard.doe@rwdit.net>
2016-01-20 09:57:26 +0000

    Fix banner seeds

--- db/seeds/banners.rb
+++ db/seeds/banners.rb
@@ -1,13 +1,14 @@
 unless Banner.find_by_default(true).present?
   ActiveRecord::Base.transaction do
-    Banner.create!(
+    banner = Banner.create!(
       title: 'This is an Alpha release of our new collections search and Music Collection',
       body: 'An Alpha release means that this website is in active development and will be updated regularly. It may sometimes be offline. Your feedback will help us improve our site.',
       link: Link.new(
         url: 'http://insights.hotjar.com/s?siteId=54631&surveyId=2939',
         text: 'Give us your input!'
-      ),
-      default: true
-    ).publish!
+      )
+    )
+    banner.publish!
+    banner.update_attributes(default: true)
   end
 end
