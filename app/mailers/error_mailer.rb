d965e52f05b10796f5fac4c0f8471082436943d9
Richard Doe <richard.doe@rwdit.net>
2016-02-18 08:34:21 +0000

    Rewrite mailer method signature

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,15 +1,15 @@
 class ErrorMailer < ApplicationMailer
   include ActionView::Helpers::TextHelper
 
-  def report(klass, message, backtrace, request_url, request_method, request_referer = nil)
+  def report(exception, request)
     fail Errors::NoRecipient unless Rails.application.config.x.error_report_mail_to.present?
 
-    @class = klass
-    @message = truncate(message, length: 1000)
-    @backtrace = Rails.backtrace_cleaner.clean(backtrace).join("\n")
-    @request_url = request_url
-    @request_method = request_method
-    @request_referer = request_referer
+    @class = exception[:class]
+    @message = truncate(exception[:message], length: 1000)
+    @backtrace = Rails.backtrace_cleaner.clean(exception[:backtrace]).join("\n")
+    @request_url = request[:original_url]
+    @request_method = request[:method]
+    @request_referer = request[:referer]
 
     mail(to: Rails.application.config.x.error_report_mail_to, subject: 'New portal error')
   end
