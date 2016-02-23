db273720d9c8951297b22d25662ba593450ed970
Richard Doe <richard.doe@rwdit.net>
2016-02-23 10:54:52 +0000

    Re #951: Raise exception on SIGTERM
    So that jobs are re-queued

---
+++ config/initializers/delayed_job.rb
@@ -0,0 +1,7 @@
+# Delayed Job configuration
+#
+# @see https://github.com/collectiveidea/delayed_job
+
+# `cf stop` on Cloud Foundry sends SIGTERM to processes.
+# Requeue the job when this occurs.
+Delayed::Worker.raise_signal_exceptions = :term
