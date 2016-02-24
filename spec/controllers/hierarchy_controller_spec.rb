6ac461d2d78044415942e903181aa02326e2d38c
Richard Doe <richard.doe@rwdit.net>
2016-02-24 11:02:33 +0000

    Spec hierarchy controller

---
+++ spec/controllers/hierarchy_controller_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe HierarchyController do
+  %w(self parent children preceding_siblings following_siblings ancestor_self_siblings).each do |action|
+    context 'when format is JSON' do
+      let(:params) { { id: 'abc/123', format: 'json' } }
+
+      describe "GET #{action}" do
+        it 'queries API' do
+          get action, params
+          expect(an_api_hierarchy_request_for('/' + params[:id])).to have_been_made
+        end
+
+        it 'returns API JSON response' do
+          get action, params
+          expect(response.content_type).to eq('application/json')
+        end
+      end
+    end
+
+    context 'when format is HTML' do
+      let(:params) { { id: 'abc/123', format: 'html' } }
+
+      describe "GET #{action}" do
+        it 'does not query API' do
+          get action, params
+          expect(an_api_hierarchy_request_for('/' + params[:id])).not_to have_been_made
+        end
+
+        it 'returns 404 status' do
+          get action, params
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+  end
+end
