42992b11bc88e327c675b3860452bdcdd1cb7bda
Richard Doe <richard.doe@rwdit.net>
2016-02-25 14:12:40 +0000

    Test DeleteOldSearchesJob

--- spec/jobs/delete_old_searches_job_spec.rb
+++ spec/jobs/delete_old_searches_job_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
-
-RSpec.describe DeleteOldSearchesJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe DeleteOldSearchesJob do
+  it 'deletes searches older than one day' do
+    expired_time = 2.days.ago
+    old_search = Search.create(created_at: expired_time, updated_at: expired_time)
+    expect { subject.perform }.to change { Search.find_by_id(old_search.id) }.to(nil)
+  end
 end
