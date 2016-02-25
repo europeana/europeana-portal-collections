f885b430c06d5b6d534fe8ace3321012165f6e45
Richard Doe <richard.doe@rwdit.net>
2016-02-25 08:43:43 +0000

    Spec Cache::ColourFacetsJob

---
+++ spec/jobs/cache/colour_facets_job_spec.rb
@@ -0,0 +1,35 @@
+require 'support/shared_examples/jobs'
+
+shared_examples 'colour facet caching job' do
+  it_behaves_like 'a caching job'
+  it_behaves_like 'an API requesting job'
+
+  it 'should write colour facets to cache' do
+    subject.perform(args)
+    cached = Rails.cache.fetch(cache_key)
+    expect(cached).to be_a(Array)
+    cached.each do |facet|
+      expect(facet).to respond_to(:hits)
+      expect(facet).to respond_to(:value)
+    end
+  end
+end
+
+RSpec.describe Cache::ColourFacetsJob do
+  context 'without collection ID' do
+    let(:cache_key) { 'browse/colours/facets' }
+    let(:args) { }
+    let(:api_request) { an_api_search_request }
+
+    it_behaves_like 'colour facet caching job'
+  end
+
+  context 'with collection ID' do
+    let(:collection) { Collection.published.first }
+    let(:cache_key) { "browse/colours/facets/#{collection.key}" }
+    let(:args) { collection.id }
+    let(:api_request) { an_api_collection_search_request(collection.id) }
+
+    it_behaves_like 'colour facet caching job'
+  end
+end
---
+++ spec/support/shared_examples/jobs.rb
@@ -0,0 +1,17 @@
+shared_examples 'a caching job' do
+  let(:cached) { Proc.new { Rails.cache.fetch(cache_key) } }
+
+  it 'writes to the cache' do
+    Rails.cache.delete(cache_key)
+    expect(cached.call).to be_nil
+    expect { subject.perform(args) }.to change { cached.call }
+    expect(cached.call).not_to be_nil
+  end
+end
+
+shared_examples 'an API requesting job' do
+  it 'should make an API request' do
+    subject.perform(args)
+    expect(api_request).to have_been_made.at_least_once
+  end
+end
