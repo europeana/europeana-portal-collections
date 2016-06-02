910007e0b73f99e2be68f1cc1838e5187b4b6b40
Richard Doe <richard.doe@rwdit.net>
2016-06-02 11:20:37 +0100

    Improve fallbacks for no Tumblr feed

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -40,14 +40,14 @@ module CollectionsHelper
     end
   end
 
-  def collection_tumblr_feed_content(options)
+  def collection_tumblr_feed_content(options = {})
     page = options[:page] || 1
     per_page = options[:per_page] || 6
 
     key = @collection.key.underscore.to_sym
     url = Cache::FeedJob::URLS[:tumblr][key]
     feed = cached_feed(url)
-    return nil if feed.entries.blank?
+    return nil if feed.blank? || feed.entries.blank?
 
     paginated_items = Kaminari.paginate_array(feed.entries).page(page).per(per_page)
 
