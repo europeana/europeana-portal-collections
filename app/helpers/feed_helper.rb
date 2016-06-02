34d92f71e1d7b08e8280ff1cddb0f068cfb53673
Richard Doe <richard.doe@rwdit.net>
2016-06-02 14:59:47 +0100

    Fix array element selection in the helper

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -14,9 +14,9 @@ module FeedHelper
   def feed_entry_img_src(item)
     [:summary, :content].each do |method|
       next unless item.send(method).present?
-      img_tag = item.send(method).match(/<img [^>]*>/i)[0]
+      img_tag = item.send(method).match(/<img [^>]*>/i)
       next unless img_tag.present?
-      url = img_tag.match(/src="(https?:\/\/[^"]*)"/i)[1]
+      url = img_tag[0].match(/src="(https?:\/\/[^"]*)"/i)[1]
       mo = MediaObject.find_by_source_url_hash(MediaObject.hash_source_url(url))
       return mo.file.url(:medium) unless mo.nil?
     end
