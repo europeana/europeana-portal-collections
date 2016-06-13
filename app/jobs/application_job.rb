b2ddaad0339d8f588f18d1a5798a8dfc8d566835
Richard Doe <richard.doe@rwdit.net>
2016-06-13 14:59:04 +0100

    Do not send error report emails
    Errors are now logged to ELK

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,15 +2,4 @@
 # Base class job background jobs
 class ApplicationJob < ActiveJob::Base
   include ActiveSupport::Benchmarkable
-
-  rescue_from(StandardError) do |exception|
-    return unless Rails.application.config.x.error_report_mail_to.present? # No email recipient configured
-
-    ErrorMailer.report_job(
-      { class: exception.class.to_s, message: exception.message, backtrace: exception.backtrace },
-      { class: self.class.to_s, arguments: arguments.inspect }
-    ).deliver_later
-
-    raise exception
-  end
 end
