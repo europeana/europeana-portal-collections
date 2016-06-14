126438d0c24d7545db428004bb87cada17fa3ebf
Richard Doe <richard.doe@rwdit.net>
2016-06-14 12:36:27 +0100

    Exclude all collection from nav

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -36,7 +36,7 @@ module CollectionsHelper
   # @return [Array<Collection>]
   def displayable_collections
     @displayable_collections ||= Collection.published.select do |c|
-      c.key.present? && c.landing_page.present? && c.landing_page.published?
+      (c.key != 'all') && c.landing_page.present? && c.landing_page.published?
     end
   end
 
