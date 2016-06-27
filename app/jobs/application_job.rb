a1ae12b68366996a695ddb90ee19054beff5ed30
Richard Doe <richard.doe@rwdit.net>
2016-06-27 10:48:42 +0100

    Do not attempt to return from a proc

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,13 +4,13 @@ class ApplicationJob < ActiveJob::Base
   include ActiveSupport::Benchmarkable
 
   rescue_from(StandardError) do |exception|
-    return unless Rails.application.config.x.error_report_mail_to.present? # No email recipient configured
+    if Rails.application.config.x.error_report_mail_to.present? # No email recipient configured
+      ErrorMailer.report_job(
+        exception: { class: exception.class.to_s, message: exception.message, backtrace: exception.backtrace },
+        job: { class: self.class.to_s, arguments: arguments.inspect }
+      ).deliver_later
 
-    ErrorMailer.report_job(
-      exception: { class: exception.class.to_s, message: exception.message, backtrace: exception.backtrace },
-      job: { class: self.class.to_s, arguments: arguments.inspect }
-    ).deliver_later
-
-    raise exception
+      raise exception
+    end
   end
 end
