8b8910e803a21fb3e8dccdfc032a974b5f0d6a8f
Richard Doe <richard.doe@rwdit.net>
2016-07-05 14:44:22 +0100

    Make the mailer work

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,13 +1,14 @@
 class FeedbackController < ApplicationController
-  respond_to :json
-
   def create
     # @todo: fail here if mailer recipient not configured?
-    args = params.slice(:text, :type, :url).merge(ip: request.ip)
-    if FeedbackMailer.send(args).deliver_later
-      respond_with(success: true)
+    if FeedbackMailer.post(text: params[:text], type: params[:type], page: params[:page], ip: request.ip).deliver_later
+      respond_to do |format|
+        format.json { render json: { success: true } }
+      end
     else
-      respond_with({ success: false }, status: 500)
+      respond_to do |format|
+        format.json { render json: { success: false }, status: 500 }
+      end
     end
   end
 end
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,10 +1,10 @@
 class FeedbackMailer < ApplicationMailer
-  def send(text: text, type: type, url: url, ip: ip)
+  def post(text: text, type: type, page: page, ip: ip)
     fail Errors::NoRecipient unless Rails.application.config.x.feedback_mail_to.present?
 
     @text = text
     @type = type
-    @url = url
+    @page = page
     @ip = ip
 
     mail(to: Rails.application.config.x.feedback_mail_to, subject: 'User feedback')
