0c359e8a2622a970c6a6b36124eb54daa636ff8e
Richard Doe <richard.doe@rwdit.net>
2016-07-07 12:07:53 +0100

    Fix spec

--- spec/models/browse_entry_spec.rb
+++ spec/models/browse_entry_spec.rb
@@ -3,10 +3,8 @@ RSpec.describe BrowseEntry do
   it { is_expected.to have_many(:pages).through(:page_elements) }
   it { is_expected.to have_and_belong_to_many(:collections) }
   it { is_expected.to belong_to(:media_object) }
-  it { is_expected.to delegate_method(:settings_category_enum).to(:class) }
   it { is_expected.to delegate_method(:file).to(:media_object) }
   it { is_expected.to accept_nested_attributes_for(:media_object) }
-  it { is_expected.to validate_inclusion_of(:settings_category).in_array(described_class.settings_category_enum) }
 
   describe '.subject_types' do
     subject { described_class.subject_types.keys }
