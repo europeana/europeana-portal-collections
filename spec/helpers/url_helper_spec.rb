5cc26df6e87f308fc186dd2d588b94f1f15bc160
Richard Doe <richard.doe@rwdit.net>
2016-07-07 09:58:51 +0100

    Switch Exhibitions URL to new foyer

--- spec/helpers/url_helper_spec.rb
+++ spec/helpers/url_helper_spec.rb
@@ -1,3 +1,13 @@
 RSpec.describe UrlHelper do
   it { is_expected.to include(Blacklight::UrlHelperBehavior) }
+
+  describe '#exhibitions_path' do
+    subject { helper.exhibitions_path }
+    it { is_expected.to eq('/en/exhibitions') }
+  end
+
+  describe '#exhibitions_foyer_path' do
+    subject { helper.exhibitions_foyer_path }
+    it { is_expected.to eq('/en/exhibitions/foyer') }
+  end
 end
