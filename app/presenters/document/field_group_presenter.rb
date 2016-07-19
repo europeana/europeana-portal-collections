7be86b831c98e40f99ed6fb42db413e49742ca9d
Richard Doe <richard.doe@rwdit.net>
2016-07-19 11:41:22 +0100

    Syntax fix

--- app/presenters/document/field_group_presenter.rb
+++ app/presenters/document/field_group_presenter.rb
@@ -26,7 +26,7 @@ module Document
     protected
 
     def format_date(text, format)
-      return text if format.nil? || (text ! =~ /^.+-/)
+      return text if format.nil? || !(text =~ /^.+-/)
       Time.parse(text).strftime(format)
     rescue ArgumentError
       text
