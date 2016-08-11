4b2c3ad9caee4f0c0a4c17691ec287e32e983cb4
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-08-11 16:33:01 +0200

    re: #1314 - add browse entry validation

--- app/views/concerns/browse_entry_displaying_view.rb
+++ app/views/concerns/browse_entry_displaying_view.rb
@@ -25,25 +25,26 @@ module BrowseEntryDisplayingView
   ##
   # @param page [Page]
   def browse_entry_items_grouped(browse_entries, page = nil)
-
-    type1 = []
-    type2 = []
+    grouped_items, type1, type2 = [], [], []
 
     browse_entries.each do |entry|
       (entry.subject_type == 'person' ? type1 : type2) << browse_entry_item(entry, page)
     end
 
-    [
-      {
-        more_link: browse_people_path(theme: collection.key),
-        more_text: t('global.navigation.more.agents'),
-        items: type1
-      },
-      {
-        more_link: browse_topics_path(theme: collection.key),
-        more_text: t('global.navigation.more.topics'),
-        items: type2
-      }
-    ]
+    if type1.count > 0
+      grouped_items << {
+                         more_link: browse_people_path(theme: collection.key),
+                         more_text: t('global.navigation.more.agents'),
+                         items: type1
+                       }
+    end
+    if type2.count > 0
+      grouped_items << {
+                         more_link: browse_topics_path(theme: collection.key),
+                         more_text: t('global.navigation.more.topics'),
+                         items: type2
+                       }
+    end
+    return grouped_items
   end
 end
