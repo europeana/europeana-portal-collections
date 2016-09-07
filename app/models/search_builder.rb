c601fdc9d42b3616808ea7d27dda63e4442be1fa
Richard Doe <richard.doe@rwdit.net>
2016-09-07 10:32:07 +0100

    Fix aliased facets

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,43 +1,26 @@
 class SearchBuilder < Europeana::Blacklight::SearchBuilder
-  ##
-  # Overrides Europeana::Blacklight::SearchBuilder method for alias handling
-  def add_facet_qf_to_api(api_parameters)
-    return unless blacklight_params[:f]
-
-    salient_facets = salient_facets_for_api_facet_qf
-
-    salient_facets.each_pair do |facet_field, value_list|
-      Array(value_list).reject(&:blank?).each do |value|
-        api_parameters[:qf] ||= []
-        api_parameters[:qf] << "#{facet_field}:" + quote_facet_value(facet_field, value)
+  def salient_facets_for_api_facet_qf
+    super.tap do |salient_facets|
+      aliased_facets_for_api_facet_qf.each_pair do |k, v|
+        salient_facets[k] ||= []
+        salient_facets[k] += v
       end
     end
   end
 
-  def salient_facets_for_api_facet_qf
-    Hash[blacklight_params[:f].map do |k, v|
-      next [nil, nil] if STANDALONE_FACETS.include?(k)
-
-      salient_facet = [k, v] if api_request_facet_fields.keys.include?(k)
-      salient_facet = [blacklight_config.facet_fields[k].aliases, v] if blacklight_config.facet_fields[k].aliases
-      salient_facet || [nil, nil]
-    end]
+  def aliased_facets_in_params
+    blacklight_config.facet_fields.select do |field_name, facet|
+      blacklight_params[:f].key?(field_name) && facet.aliases.present?
+    end
   end
 
-  ##
-  # Overrides Europeana::Blacklight::SearchBuilder method for alias handling
-  def api_request_facet_fields
-    @api_request_facet_fields ||= Hash[blacklight_config.facet_fields.map do |field_name, facet|
-      if !facet.query &&
-         (facet.include_in_request || (facet.include_in_request.nil? && blacklight_config.add_facet_fields_to_solr_request))
-        if facet.aliases.present?
-          [[facet.aliases, blacklight_config.facet_fields[facet.aliases]], [field_name,  facet]]
-        else
-          [field_name,  facet]
-        end
-      else
-        [nil, nil]
-      end
-    end]
+  def aliased_facets_for_api_facet_qf
+    aliased_facets_in_params.each_with_object({}) do |(field_name, facet), aliased|
+      aliased[facet.aliases] = blacklight_params[:f][field_name]
+    end
+  end
+
+  def requestable_facet?(facet)
+    super && !facet.aliases.present?
   end
 end
