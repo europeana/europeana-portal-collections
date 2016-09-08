82e6854e99e96c1e4afbbca7c3e0e86bc66a6f21
Richard Doe <richard.doe@rwdit.net>
2016-09-08 15:35:40 +0100

    Validate data provider URI

--- app/models/data_provider.rb
+++ app/models/data_provider.rb
@@ -1,4 +1,5 @@
 class DataProvider < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :uri, presence: true, uniqueness: true
+  validates :uri, presence: true, uniqueness: true, format:
+    { with: %r{\Ahttp://data.europeana.eu/organization/\d{6}\z} }
 end
--- spec/models/data_provider_spec.rb
+++ spec/models/data_provider_spec.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
 RSpec.describe DataProvider do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to validate_presence_of(:uri) }
   it { is_expected.to validate_uniqueness_of(:uri) }
+  it { should allow_value('http://data.europeana.eu/organization/000006').for(:uri) }
+  it { should_not allow_value('http://data.europeana.eu/organization/0006').for(:uri) }
+  it { should_not allow_value('http://europeana.eu/organization/000006').for(:uri) }
+  it { should_not allow_value('http://data.example.com/organization/000006').for(:uri) }
 end
