5eacde232a972c8236c56099e2fcf7318831bba9
Richard Doe <richard.doe@rwdit.net>
2016-09-19 16:07:13 +0100

    Resize data provider logos to 300 px wide

--- app/models/data_provider_logo.rb
+++ app/models/data_provider_logo.rb
@@ -1,6 +1,6 @@
 class DataProviderLogo < ActiveRecord::Base
   belongs_to :data_provider, inverse_of: :logo
 
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
