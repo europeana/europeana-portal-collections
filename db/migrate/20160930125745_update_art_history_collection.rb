d3a130ab88b1a4b48f5dd9be97c8597c38ff0069
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-10-06 10:18:49 +0200

    make migrtions reversible

--- db/migrate/20160930125745_update_art_history_collection.rb
+++ db/migrate/20160930125745_update_art_history_collection.rb
@@ -1,5 +1,9 @@
 class UpdateArtHistoryCollection < ActiveRecord::Migration
-  def change
+  def up
     Collection.connection.execute("UPDATE collections SET key = 'art' WHERE key ='art-history'")
+  end
+
+  def down
+    Collection.connection.execute("UPDATE collections SET key = 'art-history' WHERE key ='art'")
   end
 end
