0ed94485511f22a99db658e06f22f54a823e5561
andyjmaclean <andyjmaclean@gmail.com>
2016-10-13 16:17:13 +0200

    Re: #1575 Added background to thematic collections search bars

--- app/views/portal/index.rb
+++ app/views/portal/index.rb
@@ -3,6 +3,7 @@ module Portal
   # Portal search results view
   class Index < ApplicationView
     include SearchableView
+    include HeroImageDisplayingView
 
     def js_vars
       [{ name: 'pageName', value: 'portal/index' }]
@@ -136,6 +137,10 @@ module Portal
           }
         end
       }
+    end
+
+    def hero
+      hero_config(@landing_page.hero_image)
     end
 
     def query_terms
