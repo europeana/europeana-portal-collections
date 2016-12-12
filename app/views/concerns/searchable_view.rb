776facf2088354ea750aeec980173fe5ec35c3c6
Richard Doe <richard.doe@rwdit.net>
2016-12-12 14:32:18 +0000

    Restore entity autocomplete

--- app/views/concerns/searchable_view.rb
+++ app/views/concerns/searchable_view.rb
@@ -5,7 +5,15 @@ module SearchableView
 
   def form_search
     {
-      action: search_action_path(only_path: true)
+      action: search_action_path(only_path: true),
+      autocomplete: {
+        url: 'http://test-entity.europeana.eu/entity/suggest?wskey=apidemo&text=',
+        translations: {
+          agents: t('global.navigation.agents', default: 'People'),
+          concepts: t('global.navigation.concepts', default: 'Topics'),
+          places: t('global.navigation.places', default: 'Places')
+        }
+      }
     }
   end
 
