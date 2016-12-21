b809668e87806dddd7ba7702e024135461618077
Richard Doe <richard.doe@rwdit.net>
2016-12-21 12:51:02 +0000

    Unbreak the validation

--- app/models/browse_entry/facet_entry.rb
+++ app/models/browse_entry/facet_entry.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 class BrowseEntry
   class FacetEntry < BrowseEntry
-    validates :facet_field, inclusion: { in: :facet_field_enum }
+    validates :facet_field, inclusion: { in: :facet_field_enum_values }
     validates :facet_value, presence: true
 
     before_save :set_query
 
-    delegate :facet_field_enum, to: :class
+    delegate :facet_field_enum, :facet_field_enum_values, to: :class
 
     class << self
       def facet_field_enum
@@ -16,6 +16,10 @@ class BrowseEntry
           facet_title = presenter.facet_title || facet_field
           h[facet_title] = facet_field
         end
+      end
+
+      def facet_field_enum_values
+        facet_field_enum.values
       end
     end
 
