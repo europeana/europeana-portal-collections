e41dfaeac49ce7c11fa1697ed55e8f54a0467a58
Richard Doe <richard.doe@rwdit.net>
2017-01-18 17:00:43 +0000

    Style

--- db/migrate/20170118164140_add_newsletter_url_to_collection.rb
+++ db/migrate/20170118164140_add_newsletter_url_to_collection.rb
@@ -4,8 +4,7 @@ class AddNewsletterUrlToCollection < ActiveRecord::Migration
 
     reversible do |dir|
       dir.up do
-        fashion = Collection.find_by_key('fashion')
-        unless fashion.nil?
+        if fashion = Collection.find_by_key('fashion')
           fashion.update_attributes(newsletter_url: 'http://europeanafashion.us5.list-manage.com/subscribe?u=08acbb4918e78ab1b8b1cb158&id=eeaec60e70')
         end
       end
