ed39bf535337447eb4d442be8c4e44239837d16c
Richard Doe <richard.doe@rwdit.net>
2017-01-25 09:55:33 +0000

    Stub expectations for gallery images

--- spec/models/gallery_image_spec.rb
+++ spec/models/gallery_image_spec.rb
@@ -3,4 +3,11 @@ RSpec.describe GalleryImage do
   it { is_expected.to belong_to(:gallery).inverse_of(:images) }
   it { is_expected.to validate_presence_of(:gallery) }
   it { is_expected.to validate_presence_of(:record_url) }
+
+  it 'should validate record_url is a Europeana portal record URL'
+  it 'should remove language code from record_url'
+  it 'should remove query params from record_url'
+  it 'should enforce http:// schema on record_url'
+  it 'should validate uniqueness of record url within gallery scope'
+  it 'should queue a job to retrieve record metadata'
 end
