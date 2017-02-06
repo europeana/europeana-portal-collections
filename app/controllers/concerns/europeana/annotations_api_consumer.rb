614a5557d1d72fd16ec880b87ce28ee6c2649795
Richard Doe <richard.doe@rwdit.net>
2017-02-06 12:23:50 +0000

    Handle both minimal and standard profile annotations
    Because the Annotations API is about to change its default from minimal
    to standard.

--- app/controllers/concerns/europeana/annotations_api_consumer.rb
+++ app/controllers/concerns/europeana/annotations_api_consumer.rb
@@ -6,20 +6,42 @@ module Europeana
     extend ActiveSupport::Concern
 
     def document_annotations(document)
-      search = nil
-      begin
-        search = Europeana::API.annotation.search(annotations_api_search_params(document))
-      rescue Europeana::API::Errors::ServerError
-        return nil
+      search_response = annotations_search_for_document(document)
+      return nil unless search_response.key?('items')
+
+      annotations_from_search_response(search_response).
+        map { |annotation| annotation_text_to_display(annotation) }.compact
+    rescue Europeana::API::Errors::ServerError
+      # @todo we may not want controller actions to fail if annotations are
+      #   unavailable, but we should return something indicating that there
+      #   was a failure and perhaps indicate it to the user, e.g. as
+      #   "Annotations could not be retrieved", and/or log this for dev's to
+      #   be aware of the problem.
+    end
+
+    def annotations_search_for_document(document)
+      Europeana::API.annotation.search(annotations_api_search_params(document))
+    end
+
+    def annotations_from_search_response(search_response)
+      if annotations_search_profile_minimal?(search_response)
+        annotations_from_uris(search_response['items'])
+      else
+        search_response['items']
       end
-      responses = Europeana::API.in_parallel do |queue|
-        search.fetch('items', []).each do |item|
+    end
+
+    def annotations_search_profile_minimal?(search_response)
+      search_response['items'].any? { |item| item.is_a?(String) }
+    end
+
+    def annotations_from_uris(uris)
+      Europeana::API.in_parallel do |queue|
+        uris.each do |item|
           provider, id = item.split('/')[-2..-1]
           queue.add(:annotation, :fetch, annotations_api_fetch_params(provider, id))
         end
       end
-
-      responses.map { |annotation| annotation_text_to_display(annotation) }.compact
     end
 
     def annotation_text_to_display(annotation)
