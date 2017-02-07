98f290b488ecfa8cb41b0f7434177479430e67bd
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-02-07 16:33:09 +0100

    nicer format for range in error message

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -75,7 +75,7 @@ class Gallery < ActiveRecord::Base
   def validate_number_of_image_portal_urls
     incoming_urls = enumerable_image_portal_urls.size
     unless NUMBER_OF_IMAGES.cover?(incoming_urls)
-      errors.add(:image_portal_urls, "must include #{NUMBER_OF_IMAGES} URLs, not #{incoming_urls}")
+      errors.add(:image_portal_urls, "must include #{NUMBER_OF_IMAGES.first}-#{NUMBER_OF_IMAGES.last} URLs, not #{incoming_urls}")
     end
   end
 
