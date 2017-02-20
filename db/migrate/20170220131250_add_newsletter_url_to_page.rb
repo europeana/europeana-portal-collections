d1827a942d8c08706994cc5578b82ffdc94ac2da
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-02-20 15:36:22 +0100

    add migration

---
+++ db/migrate/20170220131250_add_newsletter_url_to_page.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class AddNewsletterUrlToPage < ActiveRecord::Migration
+  def change
+    add_column :pages, :newsletter_url, :string
+
+    reversible do |dir|
+      dir.up do
+        if fashion = Page::Landing.find_by_slug('collections/fashion')
+          fashion.update_attributes(newsletter_url: 'http://europeanafashion.us5.list-manage.com/subscribe?u=08acbb4918e78ab1b8b1cb158&id=eeaec60e70')
+        end
+      end
+    end
+  end
+end
