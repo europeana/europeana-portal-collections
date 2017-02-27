db2f83f09a81e5795a54bed5d77fc4cadb73998e
Richard Doe <richard.doe@rwdit.net>
2017-02-27 06:14:38 +0000

    Handle https:// in rights URLs

--- spec/models/edm/rights_spec.rb
+++ spec/models/edm/rights_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe EDM::Rights do
 
     context 'when rights are a recognised URI' do
       let(:rights) { 'http://creativecommons.org/licenses/by-sa/3.0/de/' }
+
       it { is_expected.to be_a(described_class) }
+
       it 'should be normalised' do
         expect(subject.id).to eq(:cc_by_sa)
+      end
+
+      context 'with https schema' do
+        let(:rights) { 'https://creativecommons.org/licenses/by-sa/3.0/de/' }
+        it 'should be detected' do
+          expect(subject).to be_a(described_class)
+          expect(subject.id).to eq(:cc_by_sa)
+        end
       end
     end
 
