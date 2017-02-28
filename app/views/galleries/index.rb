de04d8e76e5b5d32655d9995371d28491e25c966
andyjmaclean <andyjmaclean@gmail.com>
2017-02-28 16:13:00 +0100

    Re: #1964 Refactored gallery info model

--- app/views/galleries/index.rb
+++ app/views/galleries/index.rb
@@ -90,9 +90,7 @@ module Galleries
         link: gallery_path(gallery),
         count: gallery.images.size,
         images: gallery_images_content(gallery),
-        clicktip: {
-          tooltip_text: gallery.description
-        }
+        info: gallery.description
       }
     end
 
