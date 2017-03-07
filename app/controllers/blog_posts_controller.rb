f620d015b816fe8392969dd1c51520b666dd313b
Richard Doe <richard.doe@rwdit.net>
2017-03-07 10:02:45 +0100

    Comment block on blog posts controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
+##
+# Handles listing and display of blog posts retrieved from Europeana Pro via
+# JSON API.
+#
+# @todo Exception handling when `JsonApiClient` requests fail
+# @todo Extract pagination into a controller concern
 class BlogPostsController < ApplicationController
   def index
     @blog_posts = Pro::BlogPost.includes(:network).page(blog_posts_page).per(6).all
