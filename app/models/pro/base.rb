10a987591cbcad455a72cfbf9c9bcdb98e44d4de
Richard Doe <richard.doe@rwdit.net>
2017-03-08 17:40:54 +0100

    Start to populate blog listing template

--- app/models/pro/base.rb
+++ app/models/pro/base.rb
@@ -4,7 +4,7 @@ module Pro
   # Base class for JSON-API resources consumed from the Europeana Pro Bolt CMS
   # via its json-api extension.
   class Base < JsonApiClient::Resource
-    self.site = ENV['EUROPEANA_PRO_JSON_API_URL'] || 'http://pro.europeana.eu/json/'
+    self.site = (ENV['EUROPEANA_PRO_URL'] || 'http://pro.europeana.eu') + '/json/'
 
     def self.table_name
       to_s.pluralize.demodulize.downcase
--- app/views/blog_posts/index.rb
+++ app/views/blog_posts/index.rb
@@ -1,27 +1,133 @@
 # frozen_string_literal: true
 module BlogPosts
+  # @todo move the blog_item_x methods into a presenter?
   class Index < ApplicationView
+    include PaginatedView
+
     def page_title
       mustache[:page_title] ||= begin
-        ['Blog posts', site_title].join(' - ') # @todo Localeapp
+        ['Europeana Blog', site_title].join(' - ') # @todo Localeapp
       end
     end
 
-    def content
-      mustache[:content] ||= begin
+    def navigation
+      mustache[:navigation] ||= begin
         {
-          title: 'Blog posts', # @todo Localeapp
-          text: '<ol>' + @blog_posts.map { |bp| "<li>#{bp.title}#{blog_post_authors(bp)}</li>" }.join + '</ol>',
+          pagination: pagination_navigation
         }.reverse_merge(super)
       end
     end
 
+    def blog_items
+      @blog_posts.map { |post| blog_item(post) }
+    end
+
     protected
 
-    def blog_post_authors(blog_post)
-      return nil unless blog_post.respond_to?(:network) && blog_post.network.present?
+    def pagination_page_item_count
+      @blog_posts.count
+    end
 
-      author_names = blog_post.network.compact.map do |network|
+    def pagination_current_page
+      mustache[:pagination_current_page] ||= begin
+        # JsonApiClient::ResultSet#current_page always returns 1 here for some reason
+        # @blog_posts.current_page
+
+        # Get it out of the JSON API self link instead
+        current_page_api_query = URI.parse(@blog_posts.links.links['self']).query
+        Rack::Utils.parse_nested_query(current_page_api_query)['page']['number'].to_i
+      end
+    end
+
+    def pagination_per_page
+      mustache[:pagination_per_page] ||= begin
+        # JsonApiClient::ResultSet#per_page always returns number in this page
+        # here for some reason
+        # @blog_posts.per_page
+
+        # Get it out of the JSON API self link instead
+        current_page_api_query = URI.parse(@blog_posts.links.links['self']).query
+        Rack::Utils.parse_nested_query(current_page_api_query)['page']['size'].to_i
+      end
+    end
+
+    def pagination_total
+      # JsonApiClient::ResultSet#total_count always returns number in this page
+      # here for some reason
+      # @blog_posts.total_pages
+      @blog_posts.meta.total
+    end
+
+    def pagination_total_pages
+      mustache[:pagination_total_pages] ||= begin
+        # JsonApiClient::ResultSet#total_pages always returns 1 here for some reason
+        # @blog_posts.total_pages
+        (pagination_total / pagination_per_page) +
+          ((pagination_total / pagination_per_page).zero? ? 0 : 1)
+      end
+    end
+
+    def pro_blog_url(path)
+      ENV['EUROPEANA_PRO_URL'] + path
+    end
+
+    def blog_item(post)
+      {
+        author: blog_item_author(post),
+        title: post.title,
+        description: blog_item_description(post),
+        read_time: 'HOW IS READ TIME CALCULATED?',
+        date: blog_item_date(post),
+        img: blog_item_image(post),
+        tags: blog_item_tags(post),
+        label: blog_item_label(post)
+      }
+    end
+
+    def blog_item_tags(post)
+      return nil unless post.respond_to?(:taxonomy)
+      return nil unless post.taxonomy.key?(:tags) && post.taxonomy[:tags].present?
+      { items: blog_item_tags_items(post) }
+    end
+
+    def blog_item_tags_items(post)
+      post.taxonomy[:tags].map do |k, v|
+        {
+          url: pro_blog_url(k),
+          text: v
+        }
+      end
+    end
+
+    def blog_item_image(post)
+      return nil unless post.respond_to?(:image) && post.image.is_a?(Hash)
+      return nil unless post.image.key?(:thumbnail) && post.image[:thumbnail].present?
+      {
+        src: post.image[:thumbnail],
+        alt: post.image[:title]
+      }
+    end
+
+    def blog_item_date(post)
+      DateTime.strptime(post.datepublish).strftime('%-d %B, %Y') # @todo Localeapp the date format
+    end
+
+    def blog_item_description(post)
+      truncate(strip_tags(post.body), length: 350, separator: ' ')
+    end
+
+    def blog_item_label(post)
+      return nil unless post.respond_to?(:taxonomy)
+      return nil unless post.taxonomy.key?(:blogs) && post.taxonomy[:blogs].present?
+      post.taxonomy[:blogs].values.first
+    end
+
+    # @todo return actual content when template can support multiple authors
+    def blog_item_author(post)
+      return { name: 'TODO PENDING MULTI-AUTHOR SUPPORT IN TEMPLATE' }
+      return nil unless post.respond_to?(:network) && post.network.present?
+
+      author_names = post.network.compact.map do |network|
         "#{network.first_name} #{network.last_name}"
       end.join(', ')
       " (#{author_names})"
