038b9779ad4eca6468efd602447415fc8672e6be
Richard Doe <richard.doe@rwdit.net>
2017-03-21 10:29:24 +0000

    Move to_param to base Pro model

--- spec/models/pro/base_spec.rb
+++ spec/models/pro/base_spec.rb
@@ -7,4 +7,28 @@ RSpec.describe Pro::Base do
       expect(described_class.site).to eq(%(#{Pro.site}/json/))
     end
   end
+
+  describe '#to_param' do
+    before do
+      class Pro::Base::TestSubclass < Pro::Base; end
+    end
+    let(:subclass) { Pro::Base::TestSubclass }
+    subject { subclass.new }
+
+    context 'when resource has slug attr' do
+      before do
+        subject.attributes['slug'] = 'pellet'
+      end
+
+      it 'returns slug' do
+        expect(subject.to_param).to eq('pellet')
+      end
+    end
+
+    context 'when resource has no slug attr' do
+      it 'is nil' do
+        expect(subject.to_param).to be_nil
+      end
+    end
+  end
 end
