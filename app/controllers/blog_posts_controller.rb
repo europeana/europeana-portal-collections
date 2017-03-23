86412e4a9d672b40eea8a8abe1e56f13187229ca
Richard Doe <richard.doe@rwdit.net>
2017-03-23 14:36:33 +0000

    Always filter by tag "culturelover"
    And filter fashion theme by blog "Europeana Fashion Blog"

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -11,23 +11,26 @@ class BlogPostsController < ApplicationController
   self.pagination_per_default = 6
 
   def index
-    @blog_posts = themed_blog_posts.page(pagination_page).per(pagination_per).all
+    @blog_posts = Pro::BlogPost.includes(:network, :persons).
+                    where(blog_post_filters).
+                    page(pagination_page).per(pagination_per).all
     @hero_image = homepage_hero_image
     @selected_theme = blog_posts_theme
   end
 
   def show
-    @blog_post = pro_blog_posts.where(slug: params[:slug]).first
+    @blog_post = Pro::BlogPost.includes(:network, :persons).
+                   where(slug: params[:slug]).first
   end
 
   protected
 
-  def pro_blog_posts
-    Pro::BlogPost.includes(:network, :persons)
-  end
-
-  def themed_blog_posts
-    blog_posts_theme == 'all' ? pro_blog_posts : pro_blog_posts.where(tags: blog_posts_theme)
+  def blog_post_filters
+    { tags: 'culturelover' }.tap do |filters|
+      if blog_posts_theme == 'fashion'
+        filters[:blogs] = 'Europeana Fashion Blog'
+      end
+    end
   end
 
   def blog_posts_theme
