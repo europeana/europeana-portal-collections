ede4654511e36e7be5e269643507b9fb48a78fc0
andyjmaclean <andyjmaclean@gmail.com>
2017-04-04 17:16:48 +0200

    Re: #2059 Added og data

--- app/views/blog_posts/show.rb
+++ app/views/blog_posts/show.rb
@@ -1,10 +1,29 @@
 # frozen_string_literal: true
+require 'nokogiri'
+
 module BlogPosts
   class Show < ApplicationView
     def blog_title
       body_cached? ? title_from_cached_body : presenter.title
     end
     alias_method :page_content_heading, :blog_title
+
+    def head_meta
+      mustache[:head_meta] ||= begin
+        image = presenter.image(:url)
+        image = image[:src] unless image.nil?
+        description = truncate(Nokogiri::HTML(presenter.body).text, length: 200)
+
+        head_meta = [
+          { meta_name: 'description', content: description },
+          { meta_property: 'og:description', content: description },
+          { meta_property: 'og:image', content: image },
+          { meta_property: 'og:title', content: blog_title },
+          { meta_property: 'og:sitename', content: blog_title }
+        ]
+        head_meta + super
+      end
+    end
 
     def content
       mustache[:content] ||= begin
@@ -18,9 +37,28 @@ module BlogPosts
           date: presenter.date,
           introduction: presenter.introduction,
           blog_image: presenter.image(:url),
-          read_time: presenter.read_time
+          read_time: presenter.read_time,
+          social: blog_social
         }
       end
+    end
+
+    def blog_social
+      {
+        url: request.original_url,
+        facebook: {
+          text: 'Facebook'
+        },
+        twitter: {
+          text: 'Twitter'
+        },
+        pinterest: {
+          text: 'Pinterest'
+        },
+        googleplus: {
+          text: 'Google Plus'
+        }
+      }
     end
 
     def navigation
