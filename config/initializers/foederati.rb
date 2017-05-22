4b0e4f680abd66b60b6783e71de94073a31f1077
Lutz Biedinger <lutz.biedinger@gmail.com>
2017-05-22 13:10:20 +0200

    add foederati initializer

---
+++ config/initializers/foederati.rb
@@ -0,0 +1,8 @@
+# config/initializers/foederati.rb
+Foederati.configure do
+  api_keys.dpla = ENV['FEDERATED_SEARCH_API_KEYS_DPLA']
+  api_keys.digitalnz = ENV['FEDERATED_SEARCH_API_KEYS_DIGITALNZ']
+  api_keys.trove = ENV['FEDERATED_SEARCH_API_KEYS_TROVE']
+
+  defaults.limit = 4
+end
