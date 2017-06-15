6247b638dd7b93a078cd67245e6c8543888de567
Kiffin Gish <kiffin.gish@planet.nl>
2017-06-15 11:15:32 +0200

    Use safe navigation (&.) instead.

--- db/migrate/20160119093141_add_default_to_banners.rb
+++ db/migrate/20160119093141_add_default_to_banners.rb
@@ -5,7 +5,6 @@ class AddDefaultToBanners < ActiveRecord::Migration
     add_column :banners, :default, :boolean, default: false
     add_index :banners, :default
 
-    banner = Banner.find_by_key('phase-feedback')
-    banner.update_attributes(default: true) if banner
+    Banner.find_by_key('phase-feedback')&.update_attributes(default: true)
   end
 end
