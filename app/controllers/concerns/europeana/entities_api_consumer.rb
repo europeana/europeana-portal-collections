eeb59527c62813334e7dbe0d76165ad678497413
Kiffin Gish <kiffin.gish@planet.nl>
2017-07-06 16:59:34 +0200

    Wired Europeana::API to Entity API

---
+++ app/controllers/concerns/europeana/entities_api_consumer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+module Europeana
+  ##
+  # Methods for working with the Europeana Entities API
+  module EntitiesApiConsumer
+    extend ActiveSupport::Concern
+
+    def entities_api_fetch_params(type, namespace, identifier)
+      {
+        type: type,
+        namespace: namespace,
+        identifier: identifier,
+      }.reverse_merge(entities_api_env_params)
+    end
+
+    def entities_api_env_params
+      {
+        wskey: ENV['EUROPEANA_ENTITIES_API_KEY'] || Europeana::API.key,
+        api_url: ENV['EUROPEANA_ENTITIES_API_URL'] || Europeana::API.url,
+      }
+    end
+  end
+end
--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 class EntitiesController < ApplicationController
+  include Europeana::EntitiesApiConsumer
+  before_action :get_entity, only: :show
+
   def suggest
     render json: Europeana::API.entity.suggest(entity_api_params(text: params[:text]))
   end
 
   def show
-    # TODO
-    @entity = { type: params[:type], id: params[:id] }
+    respond_to do |format|
+      format.html
+      format.json { render json: @entity }
+    end
   end
 
   protected
@@ -15,5 +20,12 @@ class EntitiesController < ApplicationController
     query.merge(scope: 'europeana').tap do |api_params|
       api_params[:wskey] = ENV['EUROPEANA_ENTITIES_API_KEY'] if ENV['EUROPEANA_ENTITIES_API_KEY']
     end
+  end
+
+  private
+
+  def get_entity
+    api_fetch_params = entities_api_fetch_params(params[:type], params[:namespace], params[:identifier])
+    @entity = Europeana::API.entity.fetch(api_fetch_params)
   end
 end
