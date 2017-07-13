ca9e5f6f8cf34c2236f857ac9ffaec812c1bade1
Kiffin Gish <kiffin.gish@planet.nl>
2017-07-13 21:30:15 +0200

    Fixed hound complaints.

--- config/routes.rb
+++ config/routes.rb
@@ -64,9 +64,9 @@ Rails.application.routes.draw do
     get 'entities/suggest'
 
     constraints type: /agent|place|concept|timespan/, namespace: /base/, identifier: /\d+/ do
-      get 'entities/*type/*namespace/*identifier', as: 'entities_fetch', to: 'entities#show'
       get 'entities/*type/*namespace/*identifier/items_by', as: 'entities_fetch_items_by', to: 'entities#items_by'
       get 'entities/*type/*namespace/*identifier/items_about', as: 'entities_fetch_items_about', to: 'entities#items_about'
+      get 'entities/*type/*namespace/*identifier', as: 'entities_fetch', to: 'entities#show'
     end
 
     get 'debug/exception', to: 'debug#exception'
