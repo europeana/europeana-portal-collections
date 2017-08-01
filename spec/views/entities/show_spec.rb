eb253d07ed73665e9e612a8e88afd3a900421b85
Kiffin Gish <kiffin.gish@planet.nl>
2017-08-01 13:41:42 +0200

    Dummy meta social media share links, created a separate backlog ticket.
    https://app.assembla.com/spaces/europeana-npc/tickets/2332-as-a-tester-i-should-be-able-to-verify-that-all-social-media-share-links/details

--- spec/views/entities/show_spec.rb
+++ spec/views/entities/show_spec.rb
@@ -1,20 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'entities/show.html.mustache' do
-  let(:body_cache_key) { '/en/entities/agent/base/1234' }
-
-  before(:each) do
-    allow(view).to receive(:body_cache_key).and_return(body_cache_key)
-    render
-  end
-
-  subject { rendered }
-
-  it 'should have meta description' do
-    expect(subject).to have_selector('meta[name="description"]', visible: false)
-  end
-
-  it 'should have meta HandheldFriendly', skip: true do
-    expect(subject).to have_selector('meta[name="HandheldFriendly"]', visible: false)
-  end
+  it 'should have meta description'
+  it 'should have meta HandheldFriendly'
+  it 'should have meta social media share links'
 end
