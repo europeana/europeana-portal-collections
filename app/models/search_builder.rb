6d1d15d105e33d129eb5dc9986d2b83285bce2c3
Richard Doe <richard.doe@rwdit.net>
2017-08-01 11:48:19 +0100

    Parse qe search params for entity URI searches

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,4 +1,6 @@
 class SearchBuilder < Europeana::Blacklight::SearchBuilder
+  self.default_processor_chain << :add_entity_query_to_api
+
   def salient_facets_for_api_facet_qf
     super.tap do |salient_facets|
       aliased_facets_for_api_facet_qf.each_pair do |k, v|
@@ -22,5 +24,34 @@ class SearchBuilder < Europeana::Blacklight::SearchBuilder
 
   def requestable_facet?(facet)
     super && !facet.aliases.present?
+  end
+
+  def add_entity_query_to_api(api_parameters)
+    return unless blacklight_params[:qe]
+
+    # Keys of qe param hash will be like "agent/base/145378"
+    blacklight_params[:qe].each_pair do |entity_path, _label|
+      type, namespace, id = entity_path.split('/')
+      next if [type, namespace, id].any?(&:blank?)
+
+      query_field = search_api_query_field_for_entity_type(type)
+      next if query_field.nil?
+
+      api_parameters[:qf] ||= []
+      api_parameters[:qf] << %(#{query_field}:"http://data.europeana.eu/#{type}/#{namespace}/#{id}")
+    end
+  end
+
+  def search_api_query_field_for_entity_type(entity_type)
+    case entity_type
+    when 'agent'
+      'who'
+    when 'concept'
+      'what'
+    when 'place'
+      'where'
+    when 'timespan'
+      'when'
+    end
   end
 end
