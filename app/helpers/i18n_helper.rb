ad7fe3394c5c57c39493ebb04eab9510215565ba
Richard Doe <richard.doe@rwdit.net>
2017-08-04 11:29:24 +0100

    Update placeholder, rename method

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -40,14 +40,14 @@ module I18nHelper
 
     # Match on digits+ only, strip starting '0's and if date' length 1-3 then build "date' + CE"
     m = date.match(/^\d+$/)
-    return build_date(m[0], '+') if m && strip_leading_zeroes(m[0]).length < 4
+    return suffix_gregorian_era_date(m[0], '+') if m && strip_leading_zeroes(m[0]).length < 4
 
     # Match on '+' or '-' followed by date where date starts with digit followed by one or more digits or hyphens
     # If the matched date contains hyphens, then it must have the format 'yyyy-dd-dd'
     m = date.match(/^\s*(\+|\-)\s*(\d[\d\-]*)\s*$/i)
     if m
       year = m[2]
-      return build_date(year, m[1]) unless year.include?('-') && !year.match(/^\d{1,4}\-\d{1,2}\-\d{1,2}$/)
+      return suffix_gregorian_era_date(year, m[1]) unless year.include?('-') && !year.match(/^\d{1,4}\-\d{1,2}\-\d{1,2}$/)
     end
 
     # 0mit CE from years > 999', e.g. 'c. 1066', 'c.1066', '1066 AD' or 'c. 1066 AD' => '1066'
@@ -58,14 +58,14 @@ module I18nHelper
     # 'c. AD 46', 'a. de C. 46', 'c. 46', 'c.46', 'c.46 AD' or 'circa 46' => '46 CE'
     ['c. AD', 'a. de C.', 'c.', 'circa'].each do |abbrev|
       m = date.match(/^\s*#{abbrev}\s*(\d[\d\-]*)\s*(?:AD)?\s*$/i)
-      return build_date(m[1], '+') if m
+      return suffix_gregorian_era_date(m[1], '+') if m
     end
 
     # Match abnormal BCE dates.
     # 'About 470 BC', 'About 470 BCE', 'c.470 BCE' or 'circa 470 BCE' => '470 BCE'
     %w{About c. circa}.each do |abbrev|
       m = date.match(/^\s*#{abbrev}\s*(\d[\d\-]*)\s*(?:BCE?)?\s*$/i)
-      return build_date(m[1], '-') if m
+      return suffix_gregorian_era_date(m[1], '-') if m
     end
 
     # Otherwise just strip.
@@ -74,10 +74,10 @@ module I18nHelper
 
   private
 
-  def build_date(year, prefix)
+  def suffix_gregorian_era_date(year, prefix)
     year = strip_leading_zeroes(year)
     key = prefix == '+' ? 'current' : 'before'
-    t(key, scope: 'global.date.eras.gregorian', years: year)
+    t(key, scope: 'global.date.eras.gregorian', date: year)
   end
 
   def strip_leading_zeroes(s)
