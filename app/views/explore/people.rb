82d072c3ce8206a77232247c77d905dcff48ee34
Kiffin Gish <kiffin.gish@planet.nl>
2017-08-08 16:22:26 +0200

    Explore colours, people, periods, sources and topics views use page_content_heading instead of page_header, all tests pass.

--- app/views/explore/people.rb
+++ app/views/explore/people.rb
@@ -3,10 +3,8 @@ module Explore
     include CollectionFilterableView
     include BrowseEntryDisplayingView
 
-    def page_title
-      mustache[:page_title] ||= begin
-        [t('site.browse.people.title'), site_title].join(' - ')
-      end
+    def page_content_heading
+      t('site.browse.people.title')
     end
 
     def content
