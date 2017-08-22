876e3f943ca8a63a7aa440a0b0bff207a0f8c700
Kiffin Gish <kiffin.gish@planet.nl>
2017-08-22 13:51:02 +0200

    Subclassing pattern suggested by @rwd.

--- app/models/edm/entity.rb
+++ app/models/edm/entity.rb
@@ -1,47 +1,43 @@
 module EDM
-  class Entity < Base
-    def self.build(params)
-      type = params.delete(:type)
-      case type
-        when 'people'
-          Entities::Person.new(params)
-        when 'periods'
-          Entities::Period.new(params)
-        when 'topics'
-          Entities::Topic.new(params)
-        when 'places'
-          Entities::Place.new(params)
-      end
-    end
-  end
+  class Entity
+    include ActiveModel::Model
 
-  module Entities
-    class Base
-      attr_reader :id
+    attr_accessor :id
 
-      def initialize(params)
-        @id = params[:id]
+    class << self
+      attr_reader :human_type
+
+      def build_from_params(params)
+        subclass_for_human_type(params.delete(:type).singularize).new(params)
       end
 
-      def entity_type
-        self.class.name.sub('EDM::Entities::','').downcase
+      def subclass_for_human_type(human_type)
+        subclasses.detect { |subclass| subclass.human_type == human_type }.tap do |subclass|
+          fail ArgumentError, %(Human entity type unknown: "#{human_type}") if subclass.nil?
+        end
       end
 
-      def whoami
-        "I am a `#{entity_type}` entity with id=#{id}"
+      protected
+
+      def has_human_type(human_type)
+        @human_type = human_type
       end
     end
 
-    class Person < Base
+    class Agent < Entity
+      has_human_type 'person'
     end
 
-    class Period < Base
+    class Concept < Entity
+      has_human_type 'topic'
     end
 
-    class Place < Base
+    class Place < Entity
+      has_human_type 'place'
     end
 
-    class Topic < Base
+    class Timespan < Entity
+      has_human_type 'period'
     end
   end
 end
