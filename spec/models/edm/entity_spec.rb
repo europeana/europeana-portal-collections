0f6fe2296568876f16b87dd7b477f514949a3848
Kiffin Gish <kiffin.gish@planet.nl>
2017-08-30 09:40:56 +0200

    Moved relevant view test code in `spec/views/concerns/entity_displaying_view_spec.rb into model test code in `spec/models/edm/entity_spec.rb`

--- spec/models/edm/entity_spec.rb
+++ spec/models/edm/entity_spec.rb
@@ -24,4 +24,47 @@ RSpec.describe EDM::Entity do
       it { is_expected.to be_instance_of(EDM::Entity::Concept) }
     end
   end
+
+  describe '#entity_thumbnail_source' do
+    let(:id) { '123' }
+    let(:type) { 'person' }
+    let(:entity) do
+      JSON.parse(api_responses(:entities_fetch_agent, name: 'Entity Name', description: 'Entity Description')).
+        with_indifferent_access
+    end
+
+    subject { described_class.build_from_params(type: type, id: id) }
+
+    context 'with depiction' do
+      # it 'returns depiction source' do
+      #   expect(subject.entity_thumbnail_source).to eq(entity[:depiction][:source])
+      # end
+    end
+
+    context 'without depiction' do
+      # before do
+      #   subject.instance_variable_set(:@entity, entity.except(:depiction))
+      # end
+      #
+      # it 'is nil' do
+      #   expect(subject.entity_thumbnail_source).to be_nil
+      # end
+    end
+  end
+
+  describe '#entity_date' do
+    context 'when date has unformattable value first' do
+      # let(:date) { ['not a date', '0046'] }
+      # it 'uses and formats known value' do
+      #   expect(subject.send(:entity_date, date)).to eq('46 CE')
+      # end
+    end
+
+    context 'when date has no formattable value' do
+      # let(:date) { [' unknown 1 ', ' unknown 2 '] }
+      # it 'strips and returns first value' do
+      #   expect(subject.send(:entity_date, date)).to eq('unknown 1')
+      # end
+    end
+  end
 end
