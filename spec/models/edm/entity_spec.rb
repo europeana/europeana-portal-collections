42440030a82b8f2fc51d9cc6d3f23499be465874
Kiffin Gish <kiffin.gish@planet.nl>
2017-08-31 11:23:59 +0200

    Updated model tests (commented out), two tests fail: 1. features > entity page > type is concept 2. features > page titles > has title "Photography - Europeana Collections"

--- spec/models/edm/entity_spec.rb
+++ spec/models/edm/entity_spec.rb
@@ -33,19 +33,21 @@ RSpec.describe EDM::Entity do
         with_indifferent_access
     end
 
-    subject { described_class.build_from_params(type: type, id: id) }
+    subject { described_class.build_from_params(type: type, id: id, m: entity) }
 
     context 'with depiction' do
+      it 'returns depiction source'
       # it 'returns depiction source' do
       #   expect(subject.entity_thumbnail_source).to eq(entity[:depiction][:source])
       # end
     end
 
     context 'without depiction' do
-      # before do
-      #   subject.instance_variable_set(:@entity, entity.except(:depiction))
-      # end
-      #
+      before do
+        subject.instance_variable_set(:@entity, entity.except(:depiction))
+      end
+
+      it 'is nil'
       # it 'is nil' do
       #   expect(subject.entity_thumbnail_source).to be_nil
       # end
@@ -54,14 +56,16 @@ RSpec.describe EDM::Entity do
 
   describe '#entity_date' do
     context 'when date has unformattable value first' do
-      # let(:date) { ['not a date', '0046'] }
+      let(:date) { ['not a date', '0046'] }
+      it 'uses and formats known value'
       # it 'uses and formats known value' do
       #   expect(subject.send(:entity_date, date)).to eq('46 CE')
       # end
     end
 
     context 'when date has no formattable value' do
-      # let(:date) { [' unknown 1 ', ' unknown 2 '] }
+      let(:date) { [' unknown 1 ', ' unknown 2 '] }
+      it 'strips and returns first value'
       # it 'strips and returns first value' do
       #   expect(subject.send(:entity_date, date)).to eq('unknown 1')
       # end
