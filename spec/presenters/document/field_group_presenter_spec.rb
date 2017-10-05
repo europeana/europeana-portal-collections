1b2d2945508fa210bf566042a60d9fc07847e282
Richard Doe <richard.doe@rwdit.net>
2017-10-05 14:47:20 +0100

    Spec value exclusion from field group display

--- spec/presenters/document/field_group_presenter_spec.rb
+++ spec/presenters/document/field_group_presenter_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
   let(:document) { bl_response.documents.first }
   let(:basic_api_response) { JSON.parse(api_responses(:record, id: 'abc/123')) }
   let(:api_response) { basic_api_response }
+  let(:field_group_id) { :test_field }
 
   before do
     allow(described_class).to receive(:definition).with(field_group_id).and_return(field_definition)
@@ -16,7 +17,6 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
     subject { described_class.new(document, controller, field_group_id).display }
 
     context 'when mapping values' do
-      let(:field_group_id) { :provenance }
       let(:field_definition) do
         {
           sections: [
@@ -52,6 +52,35 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
       end
     end
 
+    context 'with excluded values' do
+      let(:excluded_values) { %w(history art) }
+      let(:field_definition) do
+        {
+          sections: [
+            {
+              exclude_vals: excluded_values,
+              fields: 'proxies.dcSubject'
+            }
+          ]
+        }
+      end
+      let(:api_response) do
+        basic_api_response.tap do |record|
+          record['object']['proxies'].first['dcSubject'] = {
+            def: %w(history music art fashion)
+          }
+        end
+      end
+
+      it 'removes excluded values' do
+        expect(subject[:sections].first[:items].none? { |item| excluded_values.include?(item[:text]) }).to be true
+      end
+
+      it 'leaves other values' do
+        expect(subject[:sections].first[:items].size).to eq(2)
+      end
+    end
+
     context 'with entities' do
       let(:entity_id) { '1234' }
       let(:field_definition) do
@@ -81,7 +110,6 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
       end
 
       context 'when entity is a Europeana agent' do
-        let(:field_group_id) { :people }
         let(:entity_type) { 'agent' }
         let(:entity_name) { 'agents' }
         let(:proxy_field) { 'dcCreator' }
@@ -92,7 +120,6 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
       end
 
       context 'when entity is a Europeana concept' do
-        let(:field_group_id) { :properties }
         let(:entity_type) { 'concept' }
         let(:entity_name) { 'concepts' }
         let(:proxy_field) { 'dcFormat' }
