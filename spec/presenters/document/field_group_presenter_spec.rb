73e41a3ae9c519c5d4c913da50425c379b63afd4
Richard Doe <richard.doe@rwdit.net>
2017-10-05 14:27:16 +0100

    Use skeletal API response fixture
    To make it clearer in spec what different inputs are being tested, and
    reduce potential proliferation of fixtures for different API responses.

--- spec/presenters/document/field_group_presenter_spec.rb
+++ spec/presenters/document/field_group_presenter_spec.rb
@@ -1,7 +1,12 @@
+# TODO: establish why "# frozen_string_literal: true" here fails in
+#       `Europeana::Blacklight::Response#force_encoding`.
+
 RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
   let(:controller) { CatalogController.new }
   let(:bl_response) { Europeana::Blacklight::Response.new(api_response, {}) }
   let(:document) { bl_response.documents.first }
+  let(:basic_api_response) { JSON.parse(api_responses(:record, id: 'abc/123')) }
+  let(:api_response) { basic_api_response }
 
   before do
     allow(described_class).to receive(:definition).with(field_group_id).and_return(field_definition)
@@ -25,7 +30,7 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
         }
       end
       let(:api_response) do
-        JSON.parse(api_responses(:record, id: 'abc/123')).tap do |record|
+        basic_api_response.tap do |record|
           record['object']['aggregations'].first['edmUgc'] = field_value
         end
       end
@@ -48,6 +53,7 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
     end
 
     context 'with entities' do
+      let(:entity_id) { '1234' }
       let(:field_definition) do
         {
           sections: [
@@ -60,26 +66,39 @@ RSpec.describe Document::FieldGroupPresenter, presenter: :field_group do
           ]
         }
       end
+      let(:api_response) do
+        basic_api_response.tap do |record|
+          record['object']['proxies'].first[proxy_field] = {
+            def: ["http://data.europeana.eu/#{entity_type}/base/#{entity_id}"]
+          }
+          record['object'][entity_name] = [
+            {
+              about: "http://data.europeana.eu/#{entity_type}/base/#{entity_id}",
+              prefLabel: { en: ['Entity label'] }
+            }
+          ]
+        end
+      end
 
       context 'when entity is a Europeana agent' do
         let(:field_group_id) { :people }
+        let(:entity_type) { 'agent' }
         let(:entity_name) { 'agents' }
         let(:proxy_field) { 'dcCreator' }
-        let(:api_response) { JSON.parse(api_responses(:record_with_entity_agent, id: 'abc/123', identifier: '1234', proxy_field: proxy_field)) }
 
         it 'links to the entity page' do
-          expect(subject[:sections].first[:items].first[:url]).to match(%r{^/en/explore/people/1234-})
+          expect(subject[:sections].first[:items].first[:url]).to eq('/en/explore/people/1234-entity-label.html')
         end
       end
 
       context 'when entity is a Europeana concept' do
         let(:field_group_id) { :properties }
+        let(:entity_type) { 'concept' }
         let(:entity_name) { 'concepts' }
         let(:proxy_field) { 'dcFormat' }
-        let(:api_response) { JSON.parse(api_responses(:record_with_entity_concept, id: 'abc/123', identifier: '1234', proxy_field: proxy_field)) }
 
         it 'links to the entity page' do
-          expect(subject[:sections].first[:items].first[:url]).to match(%r{^/en/explore/topics/1234-})
+          expect(subject[:sections].first[:items].first[:url]).to eq('/en/explore/topics/1234-entity-label.html')
         end
       end
     end
