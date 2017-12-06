ade75a81c082fb9b1cd6825842de7eeb7653f27a
Richard Doe <richard.doe@rwdit.net>
2017-12-06 09:56:41 +0100

    Revert to cookie-based session storage

---
+++ db/migrate/20171206084927_drop_sessions.rb
@@ -0,0 +1,16 @@
+class DropSessions < ActiveRecord::Migration
+  def change
+    reversible do |dir|
+      dir.down do
+        add_index :sessions, :session_id, unique: true
+        add_index :sessions, :updated_at
+      end
+    end
+
+    drop_table :sessions do |t|
+      t.string :session_id, null: false
+      t.text :data
+      t.timestamps
+    end
+  end
+end
