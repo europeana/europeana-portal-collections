2d3ef776f32d73f479cd08ccbf999d87ca97eb42
Richard Doe <richard.doe@rwdit.net>
2018-04-27 10:52:52 +0100

    Remove edm:type restriction on GalleryImage

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -3,9 +3,6 @@
 class GalleryImage < ActiveRecord::Base
   belongs_to :gallery, inverse_of: :images, touch: true
 
-  # EDM record types from which gallery images may be created
-  ALLOWED_TYPES = %w(IMAGE TEXT).freeze
-
   # Source (Search API) data from which this image is derived
   attr_accessor :source
 
@@ -47,13 +44,8 @@ class GalleryImage < ActiveRecord::Base
   def validate_source
     if source.blank?
       errors.add(:source, %(item not found by the API: "#{url}"))
-    else
-      unless source['edmIsShownBy'].present?
-        errors.add(:source, %(item has no edm:isShownBy: "#{url}"))
-      end
-      unless ALLOWED_TYPES.include?(source['type'])
-        errors.add(:source, %(item has type "#{source['type']}", not #{ALLOWED_TYPES.join(' or ')}: "#{url}"))
-      end
+    elsif source['edmIsShownBy'].blank?
+      errors.add(:source, %(item has no edm:isShownBy: "#{url}"))
     end
   end
 end
