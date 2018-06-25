f0bee29f610e2f15dd07d1a56e5ff2238bc8161f
Richard Doe <richard.doe@rwdit.net>
2018-06-25 10:29:29 +0100

    Nest class definition

--- app/models/page/error.rb
+++ app/models/page/error.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
-class Page::Error < Page
-  HTTP_ERROR_STATUS_CODES = Rack::Utils::HTTP_STATUS_CODES.keys.select { |code| (400..599).cover?(code) }
+class Page
+  class Error < Page
+    HTTP_ERROR_STATUS_CODES = Rack::Utils::HTTP_STATUS_CODES.keys.select { |code| (400..599).cover?(code) }
 
-  validates :http_code, presence: true, inclusion: { in: HTTP_ERROR_STATUS_CODES }
-  validates :http_code, uniqueness: true, unless: :exception?
+    validates :http_code, presence: true, inclusion: { in: HTTP_ERROR_STATUS_CODES }
+    validates :http_code, uniqueness: true, unless: :exception?
 
-  validates :slug, format: { with: /\A(errors|exceptions)\/.+\z/ }
+    validates :slug, format: { with: /\A(errors|exceptions)\/.+\z/ }
 
-  translates :title, :body, fallbacks_for_empty_translations: true
-  accepts_nested_attributes_for :translations, allow_destroy: true
-  default_scope { includes(:translations) }
+    translates :title, :body, fallbacks_for_empty_translations: true
+    accepts_nested_attributes_for :translations, allow_destroy: true
+    default_scope { includes(:translations) }
 
-  scope :exception, -> { where("slug LIKE 'exceptions/%'") }
-  scope :generic, -> { where("slug LIKE 'errors/%'") }
+    scope :exception, -> { where("slug LIKE 'exceptions/%'") }
+    scope :generic, -> { where("slug LIKE 'errors/%'") }
 
-  def self.for_exception(exception, http_code)
-    find_by slug: "exceptions/#{exception.class.to_s.underscore}",
-            http_code: http_code
-  end
+    def self.for_exception(exception, http_code)
+      find_by slug: "exceptions/#{exception.class.to_s.underscore}",
+              http_code: http_code
+    end
 
-  def exception?
-    slug.present? && slug.match(/\Aexceptions\//).present?
+    def exception?
+      slug.present? && slug.match(/\Aexceptions\//).present?
+    end
   end
 end
