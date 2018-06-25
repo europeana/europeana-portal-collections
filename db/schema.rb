f9925f6b64fb2af5407094bfc8a8b30d7500ed99
Richard Doe <richard.doe@rwdit.net>
2018-06-25 13:55:46 +0100

    Remove unintended schema change

--- db/schema.rb
+++ db/schema.rb
@@ -275,29 +275,6 @@ ActiveRecord::Schema.define(version: 20180625102705) do
 
   add_index "media_objects", ["source_url_hash"], name: "index_media_objects_on_source_url_hash", using: :btree
 
-  create_table "page_element_group_translations", force: :cascade do |t|
-    t.integer  "page_element_group_id", null: false
-    t.string   "locale",                null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.string   "title"
-  end
-
-  add_index "page_element_group_translations", ["locale"], name: "index_page_element_group_translations_on_locale", using: :btree
-  add_index "page_element_group_translations", ["page_element_group_id"], name: "index_page_element_group_translations_on_page_element_group_id", using: :btree
-
-  create_table "page_element_groups", force: :cascade do |t|
-    t.integer  "page_id"
-    t.integer  "position"
-    t.string   "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "page_element_groups", ["page_id"], name: "index_page_element_groups_on_page_id", using: :btree
-  add_index "page_element_groups", ["position"], name: "index_page_element_groups_on_position", using: :btree
-  add_index "page_element_groups", ["type"], name: "index_page_element_groups_on_type", using: :btree
-
   create_table "page_element_record_sets", force: :cascade do |t|
     t.integer  "page_id",       null: false
     t.string   "title",         null: false
@@ -315,10 +292,8 @@ ActiveRecord::Schema.define(version: 20180625102705) do
     t.integer "positionable_id"
     t.string  "positionable_type"
     t.integer "position"
-    t.integer "page_element_group_id"
   end
 
-  add_index "page_elements", ["page_element_group_id"], name: "index_page_elements_on_page_element_group_id", using: :btree
   add_index "page_elements", ["position"], name: "index_page_elements_on_position", using: :btree
   add_index "page_elements", ["positionable_id", "positionable_type"], name: "index_page_elements_on_positionable_id_and_positionable_type", using: :btree
 
@@ -449,9 +424,7 @@ ActiveRecord::Schema.define(version: 20180625102705) do
   add_foreign_key "federation_configs", "collections"
   add_foreign_key "galleries", "users", column: "published_by"
   add_foreign_key "gallery_images", "galleries"
-  add_foreign_key "page_element_groups", "pages"
   add_foreign_key "page_element_record_sets", "pages"
-  add_foreign_key "page_elements", "page_element_groups"
   add_foreign_key "page_elements", "pages"
   add_foreign_key "pages", "banners"
   add_foreign_key "pages", "collections"
