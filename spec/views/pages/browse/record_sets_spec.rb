52eb251200b5b81acdfdeedbe17c4b4d19e903ca
Richard Doe <richard.doe@rwdit.net>
2018-07-13 14:53:18 +0100

    Further spec the record sets browse page

--- spec/views/pages/browse/record_sets_spec.rb
+++ spec/views/pages/browse/record_sets_spec.rb
@@ -19,6 +19,34 @@ RSpec.describe 'pages/browse/record_sets.html.mustache', :common_view_components
     expect(rendered).to have_selector('title', visible: false, text: /\A#{page.title}/)
   end
 
+  it 'should have a heading for each set' do
+    render
+
+    page.sets.each do |set|
+      expect(rendered).to have_selector(%(.browse-list-title), text: set.title)
+    end
+  end
+
+  it 'should include anchors to each set' do
+    render
+
+    page.sets.each do |set|
+      href = '#' + set.title
+      text = set.title
+      expect(rendered).to have_selector(%(a[href$="#{href}"]), text: text)
+    end
+  end
+
+  it 'should link to the record page for each set member' do
+    render
+
+    page.europeana_ids.each do |record_id|
+      href = "/record#{record_id}.html"
+      text = items[record_id]['title'].first
+      expect(rendered).to have_selector(%(a[href$="#{href}"]), text: text)
+    end
+  end
+
   it 'should link to a search for each set' do
     render
 
