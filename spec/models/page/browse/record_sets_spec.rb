dbc4bf71cff50a1442544437f98df3fdf34479a7
Richard Doe <richard.doe@rwdit.net>
2018-07-13 14:08:34 +0100

    Spec #europeana_ids

--- spec/models/page/browse/record_sets_spec.rb
+++ spec/models/page/browse/record_sets_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Page::Browse::RecordSets do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to have_many(:sets).through(:elements) }
   it { is_expected.to accept_nested_attributes_for(:sets) }
+
+  describe '#europeana_ids' do
+    it 'gathers unique IDs of all sets' do
+      subject.sets.push(Europeana::Record::Set.new(europeana_ids: %w(/123/abc /456/def)))
+      subject.sets.push(Europeana::Record::Set.new(europeana_ids: %w(/456/def /789/ghi)))
+      expect(subject.europeana_ids).to eq(%w(/123/abc /456/def /789/ghi))
+    end
+  end
 end
