d4860b64bab84605f50e98061de47823257020cd
Richard Doe <richard.doe@rwdit.net>
2018-07-20 09:32:08 +0100

    Declare Page classes in migration

--- db/migrate/20180717122411_add_config_to_page.rb
+++ db/migrate/20180717122411_add_config_to_page.rb
@@ -1,29 +1,41 @@
+# frozen_string_literal: true
+
+class Page < ActiveRecord::Base
+  serialize :settings, HashWithIndifferentAccess
+  store_accessor :config, :full_width
+end
+
+class Page::Landing < Page
+  store_accessor :config, :layout_type
+end
+
+class Page::Browse::RecordSets < Page
+end
+
 class AddConfigToPage < ActiveRecord::Migration
   def change
     add_column :pages, :config, :jsonb
 
     reversible do |dir|
-      Page.send(:serialize, :settings, HashWithIndifferentAccess)
-
       dir.up do
         Page.find_each do |page|
           page.full_width = page.settings['full_width']
-          page.save!(validate: false)
+          page.save!
         end
         Page::Landing.find_each do |page|
           page.layout_type = page.settings['layout_type']
-          page.save!(validate: false)
+          page.save!
         end
       end
 
       dir.down do
         Page.find_each do |page|
           page.settings['full_width'] = page.full_width
-          page.save!(validate: false)
+          page.save!
         end
         Page::Landing.find_each do |page|
           page.settings['layout_type'] = page.layout_type
-          page.save!(validate: false)
+          page.save!
         end
       end
     end
