65562bb9b894e384460bfdbdfeed2dff198dcd34
Richard Doe <richard.doe@rwdit.net>
2018-07-20 09:54:07 +0100

    Declare model class within migration class
    And circumvent STI lookup

--- db/migrate/20180717122411_add_config_to_page.rb
+++ db/migrate/20180717122411_add_config_to_page.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
-class Page < ActiveRecord::Base
-  serialize :settings, HashWithIndifferentAccess
-  store_accessor :config, :full_width
-end
-
-class Page::Landing < Page
-  store_accessor :config, :layout_type
-end
-
-class Page::Browse::RecordSets < Page
-end
-
 class AddConfigToPage < ActiveRecord::Migration
+  class Page < ActiveRecord::Base
+    self.inheritance_column = nil
+    serialize :settings, HashWithIndifferentAccess
+    store_accessor :config, :full_width, :layout_type
+  end
+
   def change
     add_column :pages, :config, :jsonb
 
@@ -22,7 +16,7 @@ class AddConfigToPage < ActiveRecord::Migration
           page.full_width = page.settings['full_width']
           page.save!
         end
-        Page::Landing.find_each do |page|
+        Page.where(type: 'Page::Landing').find_each do |page|
           page.layout_type = page.settings['layout_type']
           page.save!
         end
@@ -33,7 +27,7 @@ class AddConfigToPage < ActiveRecord::Migration
           page.settings['full_width'] = page.full_width
           page.save!
         end
-        Page::Landing.find_each do |page|
+        Page.where(type: 'Page::Landing').find_each do |page|
           page.settings['layout_type'] = page.layout_type
           page.save!
         end
