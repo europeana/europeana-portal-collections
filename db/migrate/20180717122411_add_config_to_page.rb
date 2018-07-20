949fdf8bb4ba64101727601502d49e84e6b02d85
Richard Doe <richard.doe@rwdit.net>
2018-07-20 09:06:55 +0100

    Do not validate when saving pages during migration

--- db/migrate/20180717122411_add_config_to_page.rb
+++ db/migrate/20180717122411_add_config_to_page.rb
@@ -8,22 +8,22 @@ class AddConfigToPage < ActiveRecord::Migration
       dir.up do
         Page.find_each do |page|
           page.full_width = page.settings['full_width']
-          page.save!
+          page.save!(validate: false)
         end
         Page::Landing.find_each do |page|
           page.layout_type = page.settings['layout_type']
-          page.save!
+          page.save!(validate: false)
         end
       end
 
       dir.down do
         Page.find_each do |page|
           page.settings['full_width'] = page.full_width
-          page.save!
+          page.save!(validate: false)
         end
         Page::Landing.find_each do |page|
           page.settings['layout_type'] = page.layout_type
-          page.save!
+          page.save!(validate: false)
         end
       end
     end
