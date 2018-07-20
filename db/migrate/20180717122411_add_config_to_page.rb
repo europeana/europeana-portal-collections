5432e90f814363db0f40f3f132568095a6e28a2d
Richard Doe <richard.doe@rwdit.net>
2018-07-20 11:09:50 +0200

    Skip validation when migrating Page settings to config (#1115)
    * Do not validate when saving pages during migration
    
    * Declare Page classes in migration
    
    * Declare model class within migration class
    
    And circumvent STI lookup

--- db/migrate/20180717122411_add_config_to_page.rb
+++ db/migrate/20180717122411_add_config_to_page.rb
@@ -1,16 +1,22 @@
+# frozen_string_literal: true
+
 class AddConfigToPage < ActiveRecord::Migration
+  class Page < ActiveRecord::Base
+    self.inheritance_column = nil
+    serialize :settings, HashWithIndifferentAccess
+    store_accessor :config, :full_width, :layout_type
+  end
+
   def change
     add_column :pages, :config, :jsonb
 
     reversible do |dir|
-      Page.send(:serialize, :settings, HashWithIndifferentAccess)
-
       dir.up do
         Page.find_each do |page|
           page.full_width = page.settings['full_width']
           page.save!
         end
-        Page::Landing.find_each do |page|
+        Page.where(type: 'Page::Landing').find_each do |page|
           page.layout_type = page.settings['layout_type']
           page.save!
         end
@@ -21,7 +27,7 @@ class AddConfigToPage < ActiveRecord::Migration
           page.settings['full_width'] = page.full_width
           page.save!
         end
-        Page::Landing.find_each do |page|
+        Page.where(type: 'Page::Landing').find_each do |page|
           page.settings['layout_type'] = page.layout_type
           page.save!
         end
