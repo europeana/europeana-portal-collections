626d6b10885edb64c5f56dde94d455c258d4bd06
Lutz Biedinger <lutz.biedinger@gmail.com>
2018-08-03 17:39:44 +0200

    require namespaced js files/git ignore auto generated js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 // ...
-//= require i18n_initializer
+//= require europeana/i18n/i18n_initializer
 //= require i18n
-//= require i18n_translations
+//= require europeana/i18n/i18n_translations
