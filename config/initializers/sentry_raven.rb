493bfc9442b18dbf02284e701c1bb860ff2c463d
Richard Doe <richard.doe@rwdit.net>
2018-11-16 14:31:37 +0000

    Skip initializer if Raven not defined

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+if defined?(Raven)
+  Raven.configure do |config|
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
