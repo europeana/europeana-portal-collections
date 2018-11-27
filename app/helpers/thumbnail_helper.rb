a2a9f88628ac266816c1dc7bd456804caa6be2f5
Richard Doe <richard.doe@rwdit.net>
2018-11-27 15:58:26 +0000

    Get Contribute thumbnails from API

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -26,10 +26,6 @@ module ThumbnailHelper
   def api_thumbnail_url(**options)
     options = thumbnail_url_options_with_size(options)
 
-    if europeana_contribute_uri?(options[:uri])
-      return "#{options[:uri]}/w#{options[:size]}".sub('http://', 'https://')
-    end
-
     uri = URI.parse(api_url)
     uri.path = uri.path + '/v2/thumbnail-by-url.json'
 
@@ -38,13 +34,6 @@ module ThumbnailHelper
 
     uri.query = query.to_query
     uri.to_s
-  end
-
-  def europeana_contribute_uri?(web_resource_uri)
-    return false unless web_resource_uri.present?
-    URI.parse(web_resource_uri).host == URI.parse(Rails.application.config.x.europeana[:contribute_url]).host
-  rescue URI::InvalidURIError
-    false
   end
 
   def thumbnail_url_options_with_size(options)
